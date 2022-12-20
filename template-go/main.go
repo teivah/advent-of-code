d200271b558ddc35844034799ee334cba8d54d3b
Teiva Harsanyi <teiva.harsanyi@docker.com>
2022-12-20 01:16:08 +0100

    Adding template

---
+++ template-go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"io"
+)
+
+func fs1(input io.Reader) (int, error) {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		_ = line
+	}
+
+	return 0, nil
+}
+
+func fs2(input io.Reader) (int, error) {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		_ = line
+	}
+
+	return 0, nil
+}
