4cced7748fc3af0b344e499bfb9d49b877eee169
Teiva Harsanyi <teiva.harsanyi@docker.com>
2022-12-20 12:10:23 +0100

    First part

---
+++ day20-go/main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Ring struct {
+	len  int
+	head *Node
+	pos  map[int]*Node
+}
+
+func NewRing(s []int) *Ring {
+	previous := &Node{
+		value: s[0],
+	}
+	pos := make(map[int]*Node, len(s))
+	pos[0] = previous
+	head := previous
+	for i := 1; i < len(s); i++ {
+		v := s[i]
+		current := &Node{
+			previous: previous,
+			value:    v,
+		}
+		pos[i] = current
+		previous.next = current
+		previous = current
+	}
+	previous.next = head
+	head.previous = previous
+	return &Ring{
+		len:  len(s),
+		head: head,
+		pos:  pos,
+	}
+}
+
+func (r *Ring) move(i int) {
+	nodeA := r.pos[i]
+	to := nodeA.value
+	if to == 0 {
+		return
+	}
+
+	nodeB := nodeA
+	if to > 0 {
+		for i := 0; i < to; i++ {
+			nodeB = nodeB.next
+			if nodeB == nodeA {
+				i--
+			}
+		}
+	} else {
+		for i := to; i <= 0; i++ {
+			nodeB = nodeB.previous
+			if nodeB == nodeA {
+				i--
+			}
+		}
+	}
+
+	if nodeB.next == nodeA {
+		return
+	}
+
+	tmpPreviouA := nodeA.previous
+	tmpNextA := nodeA.next
+	tmpNextB := nodeB.next
+
+	if nodeA == r.head {
+		r.head = tmpNextA
+	} else if nodeB.next == r.head {
+		r.head = nodeA
+	}
+
+	nodeA.previous.next = nodeA.next
+	nodeA.next.previous = tmpPreviouA
+
+	nodeB.next = nodeA
+	nodeA.previous = nodeB
+
+	nodeA.next = tmpNextB
+	tmpNextB.previous = nodeA
+}
+
+func (r *Ring) getData() []int {
+	res := make([]int, 0, r.len)
+	cur := r.head
+	res = append(res, cur.value)
+	cur = cur.next
+	for cur != r.head {
+		res = append(res, cur.value)
+		cur = cur.next
+	}
+	return res
+}
+
+func (r *Ring) print() {
+	cur := r.head
+	fmt.Printf("%v ", cur.value)
+	cur = cur.next
+	for cur != r.head {
+		fmt.Printf("%v ", cur.value)
+		cur = cur.next
+	}
+	fmt.Println()
+}
+
+func (r *Ring) write() {
+	f, err := os.CreateTemp("/tmp", "v")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	cur := r.head
+	f.WriteString(fmt.Sprintf("%d\n", cur.value))
+	cur = cur.next
+	i := 0
+	for cur != r.head {
+		f.WriteString(fmt.Sprintf("%d\n", cur.value))
+		cur = cur.next
+		fmt.Println(i)
+	}
+	fmt.Println(f.Name())
+}
+
+func (r *Ring) get(s []int) []int {
+	res := make([]int, len(s))
+	for i, v := range s {
+		res[i] = r.getI(v)
+	}
+	return res
+}
+
+func (r *Ring) getI(v int) int {
+	cur := r.head
+	for {
+		if cur.value == 0 {
+			break
+		}
+		//fmt.Println(cur.value)
+		cur = cur.next
+	}
+	for i := 0; i < v; i++ {
+		cur = cur.next
+	}
+	return cur.value
+}
+
+type Node struct {
+	previous *Node
+	next     *Node
+	value    int
+}
+
+func fs1(input io.Reader, res []int) (int, error) {
+	scanner := bufio.NewScanner(input)
+	var data []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			return 0, err
+		}
+		data = append(data, i)
+	}
+
+	r := NewRing(data)
+
+	for i := 0; i < r.len; i++ {
+		//fmt.Printf("%v/%v\n", i+1, r.len)
+		r.move(i)
+	}
+
+	//r.print()
+	//r.write()
+
+	get := r.get(res)
+	fmt.Println(get)
+	sum := 0
+	for _, v := range get {
+		sum += v
+	}
+
+	return sum, nil
+}
+
+func fs2(input io.Reader) (int, error) {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		_ = line
+	}
+
+	return 0, nil
+}
---
+++ day20-go/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRing(t *testing.T) {
+	testMove(t, []int{2, 1, 3, 4}, 0, []int{1, 3, 2, 4})
+	testMove(t, []int{2, 1, -2, 8}, 2, []int{-2, 2, 1, 8})
+	testMove(t, []int{1, 2, 3, -2, 4}, 3, []int{1, -2, 2, 3, 4})
+	testMove(t, []int{1, 2, 3, 4, 5}, 2, []int{1, 3, 2, 4, 5})
+	testMove(t, []int{1, 3, 2, 4, 5}, 2, []int{2, 1, 3, 4, 5})
+	testMove(t, []int{10, 5, 1, 3}, 2, []int{1, 10, 5, 3})
+	testMove(t, []int{0, 3, 1, 10}, 1, []int{0, 3, 1, 10})
+	testMove(t, []int{0, 6, 1, 10}, 1, []int{0, 6, 1, 10})
+	testMove(t, []int{1, -3, 2, 3}, 1, []int{1, -3, 2, 3})
+	testMove(t, []int{1, -6, 2, 3}, 1, []int{1, -6, 2, 3})
+	testMove(t, []int{1, -8, 2, 3}, 1, []int{1, 2, -8, 3})
+	testMove(t, []int{1, 8, 2, 3}, 1, []int{8, 1, 2, 3})
+	testMove(t, []int{1, 10, 2, 3}, 1, []int{1, 2, 10, 3})
+}
+
+func testMove(t *testing.T, from []int, move int, to []int) {
+	r := NewRing(from)
+	r.move(move)
+	assert.Equal(t, to, r.getData())
+}
+
+func TestFs1Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	v, err := fs1(f, []int{1000, 2000, 3000})
+	require.NoError(t, err)
+	assert.Equal(t, 3, v)
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	v, err := fs1(f, []int{1000, 2000, 3000})
+	require.NoError(t, err)
+	assert.Equal(t, 42, v)
+}
+
+func TestFs2Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	v, err := fs2(f)
+	require.NoError(t, err)
+	assert.Equal(t, 42, v)
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	v, err := fs2(f)
+	require.NoError(t, err)
+	assert.Equal(t, 42, v)
+}
