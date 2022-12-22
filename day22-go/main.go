62720b09caaa74f814cf945773c6fa07ab6f1cd1
Teiva Harsanyi <teiva.harsanyi@docker.com>
2022-12-22 16:11:42 +0100

    Step 2 start

--- day22-go/main.go
+++ day22-go/main.go
@@ -106,6 +106,50 @@ func (b *Board) getDirectionValue() int {
 	}
 }
 
+func (b *Board) getLeftFace(row, col int) int {
+	if col < 0 {
+		return 0
+	}
+	pos := b.board[row][col]
+	if pos.posType == Empty {
+		return 0
+	}
+	return pos.face
+}
+
+func (b *Board) getRightFace(row, col int) int {
+	if col >= b.maxCol {
+		return 0
+	}
+	pos := b.board[row][col]
+	if pos.posType == Empty {
+		return 0
+	}
+	return pos.face
+}
+
+func (b *Board) getDownFace(row, col int) int {
+	if row >= b.maxRow {
+		return 0
+	}
+	pos := b.board[row][col]
+	if pos.posType == Empty {
+		return 0
+	}
+	return pos.face
+}
+
+func (b *Board) getUpFace(row, col int) int {
+	if row < 0 {
+		return 0
+	}
+	pos := b.board[row][col]
+	if pos.posType == Empty {
+		return 0
+	}
+	return pos.face
+}
+
 func (b *Board) left() {
 	b.curDirection.left()
 }
@@ -214,6 +258,21 @@ func (b *Board) print() {
 	}
 }
 
+func (b *Board) printFaces() {
+	fmt.Println("----------Board----------")
+	for row := 0; row < b.maxRow; row++ {
+		for col := 0; col < b.maxCol; col++ {
+			switch b.board[row][col].posType {
+			case Empty:
+				fmt.Print(" ")
+			default:
+				fmt.Printf("%d", b.board[row][col].face)
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func toBoard(lines []string) Board {
 	maxRow := len(lines)
 	maxCol := 0
@@ -306,6 +365,187 @@ func toBoard(lines []string) Board {
 	}
 }
 
+func toBoard2(lines []string, cubeSize int, hints map[int]FaceHint) Board {
+	maxRow := len(lines)
+	maxCol := 0
+	for _, line := range lines {
+		if len(line) > maxCol {
+			maxCol = len(line)
+		}
+	}
+
+	board := make([][]Position, maxRow)
+	for row := 0; row < maxRow; row++ {
+		board[row] = make([]Position, maxCol)
+		for col := 0; col < maxCol; col++ {
+			board[row][col] = Position{row: row, col: col}
+		}
+	}
+
+	for row, line := range lines {
+		runes := []rune(line)
+		for col, rune := range runes {
+			switch rune {
+			case '.':
+				board[row][col].posType = Open
+			case '#':
+				board[row][col].posType = Wall
+			}
+		}
+	}
+
+	curCol := 0
+	curRow := 0
+	for col := 0; col < maxCol; col++ {
+		if board[0][col].posType == Open {
+			curCol = col
+			break
+		}
+	}
+
+	visited := make([][]bool, maxRow)
+	for row := 0; row < maxRow; row++ {
+		visited[row] = make([]bool, maxCol)
+	}
+
+	face := 1
+	var faces []Face
+	for row := 0; row < maxRow; row++ {
+		for col := 0; col < maxCol; col++ {
+			if visited[row][col] {
+				continue
+			}
+
+			if board[row][col].posType != Empty {
+				//faces[face] = Face{row: row, col: col, value: face}
+				faces = append(faces, Face{row: row, col: col, value: face})
+				for i := 0; i < cubeSize; i++ {
+					for j := 0; j < cubeSize; j++ {
+						board[row+i][col+j].face = face
+						visited[row+i][col+j] = true
+					}
+				}
+				face++
+			}
+		}
+	}
+
+	b := Board{
+		board:  board,
+		maxRow: maxRow,
+		maxCol: maxCol,
+		curRow: curRow,
+		curCol: curCol,
+		curDirection: &Direction{
+			direction: Right,
+		},
+	}
+
+	//for faceID, hint := range hints {
+	//	face := faces[faceID]
+	//
+	//	nextFace := faces[hint.left.face]
+	//	if hint.left.direction == Left {
+	//		if !hint.left.opposite {
+	//			for row := 0; row < cubeSize; row++ {
+	//				board[face.row+row][face.col].left = &board[nextFace.row+row][nextFace.col]
+	//			}
+	//		} else {
+	//			opp := cubeSize
+	//			for row := 0; row < cubeSize; row++ {
+	//				opp--
+	//				board[face.row+row][face.col].left = &board[nextFace.row+opp][nextFace.col]
+	//			}
+	//		}
+	//	} else if hint.left.direction == Right {
+	//		if !hint.left.opposite {
+	//			for row := 0; row < cubeSize; row++ {
+	//				board[face.row+row][face.col].left = &board[nextFace.row+row][nextFace.col+cubeSize-1]
+	//			}
+	//		} else {
+	//			opp := cubeSize
+	//			for row := 0; row < cubeSize; row++ {
+	//				opp--
+	//				board[face.row+row][face.col].left = &board[nextFace.row+opp][nextFace.col+cubeSize-1]
+	//			}
+	//		}
+	//	} else if hint.left.direction == Down {
+	//		if !hint.left.opposite {
+	//			for row := 0; row < cubeSize; row++ {
+	//				board[face.row+row][face.col].left = &board[nextFace.row][nextFace.col+row]
+	//			}
+	//		} else {
+	//			opp := cubeSize
+	//			for row := 0; row < cubeSize; row++ {
+	//				opp--
+	//				board[face.row+row][face.col].left = &board[nextFace.row+opp][nextFace.col+opp]
+	//			}
+	//		}
+	//	} else {
+	//		if !hint.left.opposite {
+	//			for row := 0; row < cubeSize; row++ {
+	//				board[face.row+row][face.col].left = &board[nextFace.row][nextFace.col+row]
+	//			}
+	//		} else {
+	//			opp := cubeSize
+	//			for row := 0; row < cubeSize; row++ {
+	//				opp--
+	//				board[face.row+row][face.col].left = &board[nextFace.row+opp][nextFace.col+opp]
+	//			}
+	//		}
+	//	}
+	//
+	//}
+
+	for i, face := range faces {
+		if v := b.getLeftFace(face.row, face.col-1); v != 0 {
+			faces[i].left = v
+		}
+		if v := b.getRightFace(face.row, face.col+cubeSize); v != 0 {
+			faces[i].right = v
+		}
+		if v := b.getUpFace(face.row-1, face.col); v != 0 {
+			faces[i].up = v
+		}
+		if v := b.getDownFace(face.row+cubeSize, face.col); v != 0 {
+			faces[i].down = v
+		}
+	}
+
+	//for i := 1; i < len(faces); i++ {
+	//	face := faces[i]
+	//}
+
+	fmt.Printf("%#v\n", faces)
+
+	return b
+}
+
+type FaceHint struct {
+	left  Hint
+	right Hint
+	up    Hint
+	down  Hint
+}
+
+type Hint struct {
+	face      int
+	direction DirectionType
+	opposite  bool
+}
+
+type Face struct {
+	// Top left
+	row   int
+	col   int
+	value int
+
+	left  int
+	right int
+	up    int
+	down  int
+}
+
 type Position struct {
 	row     int
 	col     int
@@ -314,6 +554,7 @@ type Position struct {
 	right   *Position
 	up      *Position
 	down    *Position
+	face    int
 }
 
 type Type int
@@ -366,7 +607,7 @@ func toDigit(r rune) int {
 	return i
 }
 
-func fs2(input io.Reader, cubeSize int) (int, error) {
+func fs2(input io.Reader, cubeSize int, hints map[int]FaceHint) (int, error) {
 	scanner := bufio.NewScanner(input)
 	var lines []string
 	for scanner.Scan() {
@@ -374,28 +615,29 @@ func fs2(input io.Reader, cubeSize int) (int, error) {
 		lines = append(lines, line)
 	}
 
-	board := toBoard(lines[:len(lines)-2])
+	board := toBoard2(lines[:len(lines)-2], cubeSize, hints)
+	board.printFaces()
 
-	steps := toSteps(lines[len(lines)-1])
+	//steps := toSteps(lines[len(lines)-1])
 
-	for _, step := range steps {
-		if step.isMove {
-			for i := 0; i < step.move; i++ {
-				isBlocked := board.move()
-				if isBlocked {
-					break
-				}
-				//board.print()
-			}
-		} else {
-			if step.turn == Left {
-				board.left()
-			} else {
-				board.right()
-			}
-			//board.print()
-		}
-	}
+	//for _, step := range steps {
+	//	if step.isMove {
+	//		for i := 0; i < step.move; i++ {
+	//			isBlocked := board.move()
+	//			if isBlocked {
+	//				break
+	//			}
+	//			//board.print()
+	//		}
+	//	} else {
+	//		if step.turn == Left {
+	//			board.left()
+	//		} else {
+	//			board.right()
+	//		}
+	//		//board.print()
+	//	}
+	//}
 
 	res := (board.curRow+1)*1000 + (board.curCol+1)*4 + board.getDirectionValue()
 	return res, nil
--- day22-go/main_test.go
+++ day22-go/main_test.go
@@ -27,7 +27,140 @@ func TestFs1Input(t *testing.T) {
 func TestFs2Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	v, err := fs2(f, 4)
+	v, err := fs2(f, 4, map[int]FaceHint{
+		1: {
+			left: Hint{
+				face:      3,
+				direction: Up,
+				opposite:  false,
+			},
+			right: Hint{
+				face:      6,
+				direction: Right,
+				opposite:  true,
+			},
+			up: Hint{
+				face:      2,
+				direction: Up,
+				opposite:  true,
+			},
+			down: Hint{
+				face:      4,
+				direction: Up,
+				opposite:  false,
+			},
+		},
+		2: {
+			left: Hint{
+				face:      6,
+				direction: Down,
+				opposite:  true,
+			},
+			right: Hint{
+				face:      3,
+				direction: Left,
+				opposite:  false,
+			},
+			up: Hint{
+				face:      1,
+				direction: Up,
+				opposite:  true,
+			},
+			down: Hint{
+				face:      5,
+				direction: Down,
+				opposite:  true,
+			},
+		},
+		3: {
+			left: Hint{
+				face:      2,
+				direction: Right,
+				opposite:  false,
+			},
+			right: Hint{
+				face:      4,
+				direction: Left,
+				opposite:  false,
+			},
+			up: Hint{
+				face:      1,
+				direction: Left,
+				opposite:  false,
+			},
+			down: Hint{
+				face:      5,
+				direction: Left,
+				opposite:  true,
+			},
+		},
+		4: {
+			left: Hint{
+				face:      3,
+				direction: Right,
+				opposite:  false,
+			},
+			right: Hint{
+				face:      6,
+				direction: Up,
+				opposite:  true,
+			},
+			up: Hint{
+				face:      1,
+				direction: Down,
+				opposite:  false,
+			},
+			down: Hint{
+				face:      5,
+				direction: Up,
+				opposite:  false,
+			},
+		},
+		5: {
+			left: Hint{
+				face:      3,
+				direction: Down,
+				opposite:  true,
+			},
+			right: Hint{
+				face:      6,
+				direction: Left,
+				opposite:  false,
+			},
+			up: Hint{
+				face:      4,
+				direction: Down,
+				opposite:  false,
+			},
+			down: Hint{
+				face:      2,
+				direction: Down,
+				opposite:  true,
+			},
+		},
+		6: {
+			left: Hint{
+				face:      5,
+				direction: Right,
+				opposite:  false,
+			},
+			right: Hint{
+				face:      1,
+				direction: Right,
+				opposite:  true,
+			},
+			up: Hint{
+				face:      4,
+				direction: Right,
+				opposite:  true,
+			},
+			down: Hint{
+				face:      2,
+				direction: Left,
+				opposite:  true,
+			},
+		},
+	})
 	require.NoError(t, err)
 	assert.Equal(t, 42, v)
 }
@@ -35,7 +168,7 @@ func TestFs2Test(t *testing.T) {
 func TestFs2Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	v, err := fs2(f, 50)
+	v, err := fs2(f, 50, nil)
 	require.NoError(t, err)
 	assert.Equal(t, 42, v)
 }
