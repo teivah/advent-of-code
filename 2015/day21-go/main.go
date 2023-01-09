ab8d0140ca58621f4ae3462b7e8ba09d60682b91
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-09 19:48:19 +0100

    Init

--- 2015/day21-go/main.go
+++ 2015/day21-go/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"strings"
 )
 
 type Unit struct {
@@ -10,68 +12,117 @@ type Unit struct {
 	armor  int
 }
 
-/*
-damage = attacker's damage - defender's armor (at least 1)
-
-damage = 0
-armor = 0
-
-total damage or armor = sum of all the items
-100 hit points
-
-1 weapon
-0-1 armor
-0-2 rings
-
-least amount of gold
-*/
 func fs1(bossHitPoints, bossDamage, bossArmor int, storeWeapons []Unit, storeArmors []Unit, storeRings []Unit) int {
-	var rings [2]*Unit
+	equipments := allEquipments(storeWeapons, storeArmors, storeRings)
 
 	minGold := math.MaxInt
-	for _, w := range storeWeapons {
-		// With armor
-		for _, a := range storeArmors {
-			minGold = min(minGold, round(100, w, &a, rings, bossHitPoints, bossDamage, bossArmor, storeRings, w.cost+a.cost, 0))
+	for _, equipment := range equipments {
+		playerDamage, playerArmor, gold := equipment.sum(0, 0)
+
+		if win(100, playerDamage, playerArmor, bossHitPoints, bossDamage, bossArmor) {
+			minGold = min(minGold, gold)
 		}
-		// Without armor
-		minGold = min(minGold, round(100, w, nil, rings, bossHitPoints, bossDamage, bossArmor, storeRings, w.cost, 0))
 	}
 
 	return minGold
 }
 
-func round(playerHitPoints int, weapon Unit, armor *Unit, rings [2]*Unit, bossHitPoints, bossDamage, bossArmor int, storeRings []Unit, gold int, storeIndex int) int {
-	if storeIndex == len(storeRings) {
-		return math.MaxInt
+type Equipment struct {
+	weapon Unit
+	armor  *Unit
+	ring1  *Unit
+	ring2  *Unit
+}
+
+func (e Equipment) toString() string {
+	wb := strings.Builder{}
+	wb.WriteString(fmt.Sprintf("w=%v ", e.weapon.cost))
+	if e.armor != nil {
+		wb.WriteString(fmt.Sprintf(" a=%v", e.armor.cost))
+	}
+	if e.ring1 != nil {
+		wb.WriteString(fmt.Sprintf(" r1=%v", e.ring1.cost))
+	}
+	if e.ring2 != nil {
+		wb.WriteString(fmt.Sprintf(" r2=%v", e.ring2.cost))
+	}
+	return wb.String()
+}
+
+func (e Equipment) sum(sumDamage int, sumArmor int) (int, int, int) {
+	gold := 0
+
+	sumDamage += e.weapon.damage
+	sumArmor += e.weapon.armor
+	gold = e.weapon.cost
+
+	if e.armor != nil {
+		sumDamage += e.armor.damage
+		sumArmor += e.armor.armor
+		gold += e.armor.cost
+	}
+	if e.ring1 != nil {
+		sumDamage += e.ring1.damage
+		sumArmor += e.ring1.armor
+		gold += e.ring1.cost
+	}
+	if e.ring2 != nil {
+		sumDamage += e.ring2.damage
+		sumArmor += e.ring2.armor
+		gold += e.ring2.cost
 	}
 
-	curRings := 0
-	if rings[0] == nil {
-		curRings = 0
-	} else if rings[1] == nil {
-		curRings = 1
-	} else {
-		curRings = 2
+	return sumDamage, sumArmor, gold
+}
+
+func allEquipments(storeWeapons []Unit, storeArmors []Unit, storeRings []Unit) []Equipment {
+	var equipments []Equipment
+
+	for _, w := range storeWeapons {
+		// With armor
+		for _, a := range storeArmors {
+			a := a
+			equipments = append(equipments, all(w, &a, nil, nil, storeRings, 0)...)
+		}
+		equipments = append(equipments, all(w, nil, nil, nil, storeRings, 0)...)
 	}
+
+	return equipments
+}
+
+func all(weapon Unit, armor, ring1, ring2 *Unit, storeRings []Unit, storeIndex int) []Equipment {
+	if storeIndex == len(storeRings) {
+		return nil
+	}
+
+	if ring1 != nil && ring2 != nil {
+		return nil
+	}
+
+	var equipments []Equipment
 
 	// With
-	with := math.MaxInt
-	if curRings < 2 {
-		rings[curRings] = &storeRings[storeIndex]
-		playerDamage, playerArmor := sum(weapon, armor, rings)
-		if win(playerHitPoints, playerDamage, playerArmor, bossHitPoints, bossDamage, bossArmor) {
-			with = gold + storeRings[storeIndex].cost
-		} else {
-			with = round(playerHitPoints, weapon, armor, rings, bossHitPoints, bossDamage, bossArmor, storeRings, gold+storeRings[storeIndex].cost, storeIndex+1)
-		}
-		rings[curRings] = nil
+	if ring1 == nil {
+		equipments = append(equipments, Equipment{
+			weapon: weapon,
+			armor:  armor,
+			ring1:  &storeRings[storeIndex],
+		})
+		equipments = append(equipments, all(weapon, armor, &storeRings[storeIndex], nil, storeRings, storeIndex+1)...)
+	} else {
+		equipments = append(equipments, Equipment{
+			weapon: weapon,
+			armor:  armor,
+			ring1:  ring1,
+			ring2:  &storeRings[storeIndex],
+		})
+		equipments = append(equipments, all(weapon, armor, ring1, &storeRings[storeIndex], storeRings, storeIndex+1)...)
 	}
 
 	// Without
-	without := round(playerHitPoints, weapon, armor, rings, bossHitPoints, bossDamage, bossArmor, storeRings, gold, storeIndex+1)
+	equipments = append(equipments, all(weapon, armor, ring1, ring2, storeRings, storeIndex+1)...)
 
-	return min(with, without)
+	return equipments
 }
 
 func min(a, b int) int {
@@ -81,38 +132,19 @@ func min(a, b int) int {
 	return b
 }
 
-func sum(weapon Unit, armor *Unit, rings [2]*Unit) (int, int) {
-	sumDamage := 0
-	sumArmor := 0
-
-	sumDamage += weapon.damage
-	sumArmor += weapon.armor
-	if armor != nil {
-		sumDamage += armor.damage
-		sumArmor += armor.armor
-	}
-	for _, ring := range rings {
-		if ring != nil {
-			sumDamage += ring.damage
-			sumArmor += ring.armor
-		}
-	}
-
-	return sumDamage, sumArmor
-}
-
 func win(playerHitPoints, playerDamage, playerArmor, bossHitPoints, bossDamage, bossArmor int) bool {
+	pd := calcDamage(playerDamage, bossArmor)
+	bd := calcDamage(bossDamage, playerArmor)
+
 	for {
 		// Player
-		d := calcDamage(playerDamage, bossArmor)
-		bossHitPoints -= d
+		bossHitPoints -= pd
 		if bossHitPoints <= 0 {
 			return true
 		}
 
 		// Boss
-		d = calcDamage(bossDamage, playerArmor)
-		playerHitPoints -= d
+		playerHitPoints -= bd
 		if playerHitPoints <= 0 {
 			return false
 		}
@@ -128,5 +160,33 @@ func calcDamage(damage, armor int) int {
 }
 
 func fs2(bossHitPoints, bossDamage, bossArmor int, storeWeapons []Unit, storeArmors []Unit, storeRings []Unit) int {
-	return 0
+	equipments := allEquipments(storeWeapons, storeArmors, storeRings)
+
+	atLeastOneWin := make(map[int]bool)
+	for _, equipment := range equipments {
+		playerDamage, playerArmor, gold := equipment.sum(0, 0)
+
+		for _, bossEquipment := range equipments {
+			bossDamage, bossArmor, _ := bossEquipment.sum(bossDamage, bossArmor)
+
+			if !win(100, playerDamage, playerArmor, bossHitPoints, bossDamage, bossArmor) {
+				if _, exists := atLeastOneWin[gold]; !exists {
+					atLeastOneWin[gold] = false
+				}
+			} else {
+				atLeastOneWin[gold] = true
+			}
+		}
+	}
+
+	fmt.Println(atLeastOneWin)
+
+	maxGold := 0
+	for gold, oneWin := range atLeastOneWin {
+		if !oneWin && gold > maxGold {
+			maxGold = gold
+		}
+	}
+
+	return maxGold
 }
