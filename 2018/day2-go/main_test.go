f50ce76c35daf9f0ee5166443afc5ca057708ff4
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-21 08:45:05 +0100

    2018: day 2

---
+++ 2018/day2-go/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 12, fs1(f))
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 6888, fs1(f))
+}
+
+func TestFs2Test(t *testing.T) {
+	assert.Equal(t, "fgij", fs2(strings.NewReader(`abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`)))
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, "", fs2(f))
+}
