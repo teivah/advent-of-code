da3b899d332f6773dc46a11571db799f00c27883
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-22 20:36:32 +0100

    2018: day 6

--- 2018/day6-go/main.go
+++ 2018/day6-go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"strings"
 
 	lib "github.com/teivah/advent-of-code"
@@ -16,7 +17,105 @@ func fs1(input io.Reader, rounds int) int {
 		coords = append(coords, toCoord(scanner.Text()))
 	}
 
-	return bfs(coords, rounds)
+	return draw(coords)
+}
+
+func draw(coords []Coord) int {
+	minerCol := lib.NewMiner()
+	minerRow := lib.NewMiner()
+	maxerRow := lib.NewMaxer()
+	maxerCol := lib.NewMaxer()
+
+	for _, coord := range coords {
+		minerCol.Add(coord.col)
+		maxerCol.Add(coord.col)
+		minerRow.Add(coord.row)
+		maxerRow.Add(coord.row)
+	}
+
+	minCol := minerCol.Get()
+	minRow := minerRow.Get()
+	maxCol := maxerCol.Get()
+	maxRow := maxerRow.Get()
+
+	const buffer = 100
+
+	grid := make(map[Coord]*Entry)
+	for row := minRow - buffer; row < maxRow+buffer+1; row++ {
+		for col := minCol - buffer; col < maxCol+buffer+1; col++ {
+			grid[Coord{col, row}] = &Entry{dst: math.MaxInt, id: -1}
+		}
+	}
+
+	for id, coord := range coords {
+		for row := minRow - buffer; row < maxRow+buffer+1; row++ {
+			for col := minCol - buffer; col < maxCol+buffer+1; col++ {
+				dst := lib.Abs(row-coord.row) + lib.Abs(col-coord.col)
+				key := Coord{col, row}
+				if dst < grid[key].dst {
+					grid[key].dst = dst
+					grid[key].id = id
+				} else if dst == grid[key].dst {
+					grid[key].id = -1
+				}
+			}
+		}
+	}
+
+	count := make(map[int]int)
+	for row := minRow - buffer; row < maxRow+buffer+1; row++ {
+		for col := minCol - buffer; col < maxCol+buffer+1; col++ {
+			count[grid[Coord{col, row}].id]++
+		}
+	}
+
+	dontConsider := make(map[int]struct{})
+	for id, coord := range coords {
+		visited := make(map[Coord]struct{})
+		if keepGrowing(grid, id, coord, visited) {
+			dontConsider[id] = struct{}{}
+		}
+	}
+
+	maxer := lib.NewMaxer()
+	for id, v := range count {
+		if id == -1 {
+			continue
+		}
+		if _, exists := dontConsider[id]; exists {
+			continue
+		}
+		maxer.Add(v)
+	}
+
+	return maxer.Get()
+}
+
+func keepGrowing(grid map[Coord]*Entry, id int, coord Coord, visited map[Coord]struct{}) bool {
+	_, exists := visited[coord]
+	if exists {
+		return false
+	}
+	visited[coord] = struct{}{}
+
+	v, exists := grid[coord]
+	if !exists {
+		return true
+	}
+
+	if v.id != id {
+		return false
+	}
+
+	return keepGrowing(grid, id, coord.delta(-1, 0), visited) ||
+		keepGrowing(grid, id, coord.delta(1, 0), visited) ||
+		keepGrowing(grid, id, coord.delta(0, -1), visited) ||
+		keepGrowing(grid, id, coord.delta(0, 1), visited)
+}
+
+type Entry struct {
+	dst int
+	id  int
 }
 
 func bfs(coords []Coord, rounds int) int {
@@ -32,6 +131,9 @@ func bfs(coords []Coord, rounds int) int {
 	for i := 0; i < rounds; i++ {
 		size := len(q)
 
+		fmt.Println(i, len(q))
+		roundGrid := make(map[Coord]int)
+
 		for j := 0; j < size; j++ {
 			if len(q) == 0 {
 				break
@@ -40,10 +142,19 @@ func bfs(coords []Coord, rounds int) int {
 			s := q[0]
 			q = q[1:]
 
-			_, exists := grid[s.coord]
-			if !exists {
-				grid[s.coord] = s.id
-				q = append(q, s)
+			if _, exists := grid[s.coord]; exists {
+				continue
+			}
+
+			v, exists := roundGrid[s.coord]
+			if exists {
+				if v != s.id {
+					roundGrid[s.coord] = -1
+				} else if v == s.id {
+					continue
+				}
+			} else {
+				roundGrid[s.coord] = s.id
 			}
 
 			q = append(q, State{s.id, s.coord.delta(-1, 0)})
@@ -52,10 +163,21 @@ func bfs(coords []Coord, rounds int) int {
 			q = append(q, State{s.id, s.coord.delta(0, 1)})
 		}
 
-		previous = counts
-		counts = make(map[int]int)
-		for _, v := range grid {
-			counts[v]++
+		for k, v := range roundGrid {
+			_, exists := grid[k]
+			if exists {
+				continue
+			}
+
+			grid[k] = v
+		}
+
+		if i >= rounds-2 {
+			previous = counts
+			counts = make(map[int]int)
+			for _, v := range grid {
+				counts[v]++
+			}
 		}
 	}
 
@@ -66,8 +188,6 @@ func bfs(coords []Coord, rounds int) int {
 			notExpanding[k] = struct{}{}
 		}
 	}
-
-	//printGrid(grid)
 
 	sum := make(map[int]int)
 	for _, v := range grid {
@@ -83,7 +203,7 @@ func bfs(coords []Coord, rounds int) int {
 	return max
 }
 
-func printGrid(grid map[Coord]int) {
+func printGrid(grid map[Coord]*Entry) {
 	minerRow := lib.NewMiner()
 	minerCol := lib.NewMiner()
 	maxerRow := lib.NewMaxer()
@@ -101,10 +221,10 @@ func printGrid(grid map[Coord]int) {
 			if !exists {
 				fmt.Printf(" ")
 			} else {
-				if v == -1 {
+				if v.id == -1 {
 					fmt.Printf(".")
 				} else {
-					fmt.Printf("%d", v)
+					fmt.Printf("%d", v.id)
 				}
 			}
 		}
@@ -138,12 +258,61 @@ func toCoord(s string) Coord {
 	}
 }
 
-func fs2(input io.Reader) int {
+func fs2(input io.Reader, less int) int {
 	scanner := bufio.NewScanner(input)
+	var coords []Coord
 	for scanner.Scan() {
-		line := scanner.Text()
-		_ = line
+		coords = append(coords, toCoord(scanner.Text()))
 	}
 
-	return 42
+	return region(coords, less)
+}
+
+func region(coords []Coord, less int) int {
+	minerCol := lib.NewMiner()
+	minerRow := lib.NewMiner()
+	maxerRow := lib.NewMaxer()
+	maxerCol := lib.NewMaxer()
+
+	for _, coord := range coords {
+		minerCol.Add(coord.col)
+		maxerCol.Add(coord.col)
+		minerRow.Add(coord.row)
+		maxerRow.Add(coord.row)
+	}
+
+	minCol := minerCol.Get()
+	minRow := minerRow.Get()
+	maxCol := maxerCol.Get()
+	maxRow := maxerRow.Get()
+
+	const buffer = 100
+
+	grid := make(map[Coord]int)
+	for row := minRow - buffer; row < maxRow+buffer+1; row++ {
+		for col := minCol - buffer; col < maxCol+buffer+1; col++ {
+			grid[Coord{col, row}] = 0
+		}
+	}
+
+	for _, coord := range coords {
+		for row := minRow - buffer; row < maxRow+buffer+1; row++ {
+			for col := minCol - buffer; col < maxCol+buffer+1; col++ {
+				dst := lib.Abs(row-coord.row) + lib.Abs(col-coord.col)
+				key := Coord{col, row}
+				grid[key] += dst
+			}
+		}
+	}
+
+	count := 0
+	for row := minRow - buffer; row < maxRow+buffer+1; row++ {
+		for col := minCol - buffer; col < maxCol+buffer+1; col++ {
+			if grid[Coord{col, row}] < less {
+				count++
+			}
+		}
+	}
+
+	return count
 }
--- 2018/day6-go/main_test.go
+++ 2018/day6-go/main_test.go
@@ -11,23 +11,23 @@ import (
 func TestFs1Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 17, fs1(f, 10))
+	assert.Equal(t, 17, fs1(f, 14))
 }
 
 func TestFs1Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 42, fs1(f, 20))
+	assert.Equal(t, 3251, fs1(f, 30))
 }
 
 func TestFs2Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 42, fs2(f))
+	assert.Equal(t, 16, fs2(f, 32))
 }
 
 func TestFs2Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 42, fs2(f))
+	assert.Equal(t, 47841, fs2(f, 10000))
 }
