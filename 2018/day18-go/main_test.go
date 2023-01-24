9ed91d7da90e00a78e3e8a1f871096d329e8a98d
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-24 23:30:22 +0100

    2018: day 18

---
+++ 2018/day18-go/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 1147, fs1(f))
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 646437, fs1(f))
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 208080, fs2(f))
+}
--- README.md
+++ README.md
@@ -44,6 +44,7 @@
 * [Day 15](https://adventofcode.com/2018/day/15): [Go](2018/day15-go/main.go)
 * [Day 16](https://adventofcode.com/2018/day/16): [Go](2018/day16-go/main.go)
 * [Day 17](https://adventofcode.com/2018/day/17): [Go](2018/day17-go/main.go)
+* [Day 18](https://adventofcode.com/2018/day/18): [Go](2018/day18-go/main.go)
 
 ## 2017
 
