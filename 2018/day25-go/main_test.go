89312d8021250117a0713277a40992b5adee376c
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-27 01:15:34 +0100

    First

---
+++ 2018/day25-go/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFs1Test_0(t *testing.T) {
+	assert.Equal(t, 2, fs1(strings.NewReader(`0,0,0,0
+3,0,0,0
+0,3,0,0
+0,0,3,0
+0,0,0,3
+0,0,0,6
+9,0,0,0
+12,0,0,0`)))
+}
+
+func TestFs1Test_1(t *testing.T) {
+	assert.Equal(t, 4, fs1(strings.NewReader(`-1,2,2,0
+0,0,2,-2
+0,0,0,-2
+-1,2,0,0
+-2,-2,-2,2
+3,0,2,-1
+-1,3,2,2
+-1,0,-1,0
+0,2,1,-2
+3,0,0,0`)))
+}
+
+func TestFs1Test_2(t *testing.T) {
+	assert.Equal(t, 3, fs1(strings.NewReader(`1,-1,0,1
+2,0,-1,0
+3,2,-1,0
+0,0,3,1
+0,0,-1,-1
+2,3,-2,0
+-2,2,0,0
+2,-2,0,-1
+1,-1,0,-1
+3,2,0,2`)))
+}
+
+func TestFs1Test_3(t *testing.T) {
+	assert.Equal(t, 8, fs1(strings.NewReader(`1,-1,-1,-2
+-2,-2,0,1
+0,2,1,3
+-2,3,-2,1
+0,2,3,-2
+-1,-1,1,-2
+0,-2,-1,0
+-2,2,3,-1
+1,2,2,0
+-1,-2,0,-2`)))
+}
+
+//
+//func TestFs1Input(t *testing.T) {
+//	f, err := os.Open("input.txt")
+//	require.NoError(t, err)
+//	assert.Equal(t, 42, fs1(f))
+//}
