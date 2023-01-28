4a2b8f5376a52a8e5175cd3d7a86658dfcd839ea
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-28 22:24:48 +0100

    Lib

--- lib.go
+++ lib.go
@@ -230,6 +230,40 @@ const (
 	Right
 )
 
+func (d Direction) Turn(turn Direction) Direction {
+	if turn == Up || turn == Down {
+		return turn
+	}
+
+	switch d {
+	case Up:
+		return turn
+	case Down:
+		switch turn {
+		case Left:
+			return Right
+		case Right:
+			return Left
+		}
+	case Left:
+		switch turn {
+		case Left:
+			return Down
+		case Right:
+			return Up
+		}
+	case Right:
+		switch turn {
+		case Left:
+			return Up
+		case Right:
+			return Down
+		}
+	}
+
+	panic("not handled")
+}
+
 type Position struct {
 	Row int
 	Col int
