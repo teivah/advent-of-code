76b4dd12349ad9a4286dd33fc9c05adbb00d18b6
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-01-29 19:17:35 +0100

    Checkpoint

---
+++ 2019/day14-go/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+
+	lib "github.com/teivah/advent-of-code"
+)
+
+func fs1(input io.Reader) int {
+	scanner := bufio.NewScanner(input)
+	transformations := make(map[string]Transformation)
+	for scanner.Scan() {
+		t := toTransformation(scanner.Text())
+		transformations[t.to.name] = t
+	}
+
+	v, _ := min(Unit{"FUEL", 1}, transformations, make(map[string]Unit))
+	return v
+}
+
+func min(unit Unit, transformations map[string]Transformation, remaining map[string]Unit) (int, int) {
+	if unit.name == "ORE" {
+		return unit.count, 0
+	}
+
+	if v, exists := remaining[unit.name]; exists {
+		if v.count >= unit.count {
+			del(remaining, v.count-unit.count, unit.name)
+			return 0, 0
+		}
+	}
+
+	t := transformations[unit.name]
+
+	n := 1
+	for ; ; n++ {
+		if n*t.to.count >= unit.count {
+			break
+		}
+	}
+	// We need to repeat the process n times
+
+	v := n*t.to.count - unit.count
+	remaining = cpy(remaining)
+
+	sum := 0
+	for _, needed := range t.from {
+		count, r := min(needed, transformations, remaining)
+		add(remaining, r, needed.name)
+		sum += count
+	}
+
+	return sum, v
+}
+
+func cpy(remaining map[string]Unit) map[string]Unit {
+	res := make(map[string]Unit, len(remaining))
+	for k, v := range remaining {
+		res[k] = v
+	}
+	return res
+}
+
+func add(remaining map[string]Unit, count int, name string) {
+	unit, exists := remaining[name]
+	if !exists {
+		remaining[name] = Unit{
+			name:  name,
+			count: count,
+		}
+		return
+	}
+
+	unit.count += count
+	remaining[name] = unit
+}
+
+func del(remaining map[string]Unit, count int, name string) {
+	unit := remaining[name]
+	if unit.count == count {
+		delete(remaining, name)
+		return
+	}
+	unit.count -= count
+	remaining[name] = unit
+}
+
+func toTransformation(s string) Transformation {
+	s = strings.TrimSpace(s)
+
+	start := 0
+	var from []Unit
+	for {
+		if s[start] == '=' {
+			break
+		}
+
+		i := start + 1
+		for ; ; i++ {
+			r := rune(s[i])
+			if !isDigit(r) {
+				break
+			}
+		}
+		count := lib.StringToInt(s[start:i])
+
+		start = i + 1
+		i = start + 1
+		for ; ; i++ {
+			r := rune(s[i])
+			if !isLetter(r) {
+				break
+			}
+		}
+		name := s[start:i]
+		from = append(from, Unit{
+			name:  name,
+			count: count,
+		})
+		start = i
+		if s[start] == ',' {
+			start = i + 2
+		} else {
+			start = i + 4
+			break
+		}
+	}
+
+	del := lib.NewDelimiter(s[start:], " ")
+	return Transformation{
+		from: from,
+		to: Unit{
+			count: del.GetInt(0),
+			name:  del.GetString(1),
+		},
+	}
+}
+
+func isDigit(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+func isLetter(r rune) bool {
+	return r >= 'A' && r <= 'Z'
+}
+
+type Transformation struct {
+	from []Unit
+	to   Unit
+}
+
+type Unit struct {
+	name  string
+	count int
+}
+
+func (u Unit) String() string {
+	return fmt.Sprintf("%s:%v", u.name, u.count)
+}
+
+func fs2(input io.Reader) int {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		_ = line
+	}
+
+	return 42
+}
---
+++ 2019/day14-go/main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Test(t *testing.T) {
+	//assert.Equal(t, 31, fs1(strings.NewReader(`10 ORE => 10 A
+	//1 ORE => 1 B
+	//7 A, 1 B => 1 C
+	//7 A, 1 C => 1 D
+	//7 A, 1 D => 1 E
+	//7 A, 1 E => 1 FUEL`)))
+
+	assert.Equal(t, 165, fs1(strings.NewReader(`9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL`)))
+
+	//assert.Equal(t, 10, fs1(strings.NewReader(`10 ORE => 1 FUEL`)))
+	//assert.Equal(t, 100, fs1(strings.NewReader(`10 ORE => 10 A
+	//100 A => 1 FUEL`)))
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 42, fs1(f))
+}
+
+func TestFs2Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 42, fs2(f))
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 42, fs2(f))
+}
