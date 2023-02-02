e771f9b7eb4e38c04de02a14f0f033cdda20fb4b
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-02-03 00:19:01 +0100

    2020: Day 2

---
+++ 2020/day2-go/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 2, fs1(f))
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 519, fs1(f))
+}
+
+func TestFs2Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 1, fs2(f))
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 708, fs2(f))
+}
+
+func Test_toPolicy(t *testing.T) {
+	assert.Equal(t, Policy{
+		from:   15,
+		to:     16,
+		letter: 'k',
+		s:      "kpvfgsrkptkkbdkj",
+	}, toPolicy(`15-16 k: kpvfgsrkptkkbdkj`))
+}
