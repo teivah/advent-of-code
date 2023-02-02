46933a79de5e3abac15927a07f3fb9993bcba472
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-02-02 10:36:06 +0100

    Commit

---
+++ 2019/day23-go/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+
+	assert.Equal(t, 23626, fs(f))
+}
