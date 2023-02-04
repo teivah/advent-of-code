d9341f4856e1cb3e2379cb774bdaee33aeedf2e5
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-02-04 14:10:16 +0100

    2020 Day 14

---
+++ 2020/day14-go/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 165, fs1(f))
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 42, fs1(f))
+}
+
+func TestFs2Test(t *testing.T) {
+	assert.Equal(t, 208, fs2(strings.NewReader(`mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`)))
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 42, fs2(f))
+}
