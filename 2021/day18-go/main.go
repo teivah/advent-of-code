b176b013378266b791a9dd621ec5fc5f6b788954
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-02-10 00:16:22 +0100

    Checkpoint

--- 2021/day18-go/main.go
+++ 2021/day18-go/main.go
@@ -4,35 +4,67 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"strconv"
 	"strings"
 
 	aoc "github.com/teivah/advent-of-code"
 )
 
-func fs1(input io.Reader) int {
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		pair, _ := parse(line, 1)
-		_ = pair
+func fs1(input io.Reader) string {
+	lines := aoc.ReaderToStrings(input)
+	res := lines[0]
+	for i := 1; i < len(lines); i++ {
+		res = add(res, lines[i])
+		res = reduction(res)
 	}
 
-	return 42
+	return res
+}
+
+func reduction(s string) string {
+	for {
+		v, change := explode(s)
+		if change {
+			s = v
+			continue
+		}
+
+		v, change = split(s)
+		if change {
+			s = v
+			continue
+		}
+
+		break
+	}
+
+	return s
 }
 
 func add(s1, s2 string) string {
-	return fmt.Sprintf("[%s,%s]")
+	return fmt.Sprintf("[%s,%s]", s1, s2)
 }
 
-func explode(s string) string {
+func split(s string) (string, bool) {
+	for i := 1; i < len(s); i++ {
+		if aoc.IsRuneDecimal(rune(s[i])) && aoc.IsRuneDecimal(rune(s[i-1])) {
+			v := aoc.StringToInt(s[i-1 : i+1])
+			return transformString(s, i-1, i+1, fmt.Sprintf("[%d,%d]",
+				int(math.Floor(float64(v)/2.)), int(math.Ceil(float64(v)/2.)))), true
+		}
+	}
+	return s, false
+}
+
+func explode(s string) (string, bool) {
 	res := ""
 
 	open := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == '[' {
 			open++
-			if open == 5 {
+			if open >= 5 {
 				r1 := rune(s[i+1])
 				r2 := rune(s[i+3])
 				if aoc.IsRuneDecimal(r1) && aoc.IsRuneDecimal(r2) {
@@ -44,8 +76,12 @@ func explode(s string) string {
 					for left := i - 1; left >= 0; left-- {
 						r := rune(s[left])
 						if aoc.IsRuneDecimal(r) {
-							v := strconv.Itoa(aoc.RuneToInt(r) + x)
+							inc := aoc.RuneToInt(r) + x
+							v := strconv.Itoa(inc)
 							s = transformString(s, left, left+1, v)
+							if inc > 9 {
+								i++
+							}
 							break
 						}
 					}
@@ -59,7 +95,7 @@ func explode(s string) string {
 						}
 					}
 
-					return s
+					return s, true
 				}
 			}
 		} else if s[i] == ']' {
@@ -67,21 +103,12 @@ func explode(s string) string {
 		}
 	}
 
-	return res
+	return res, false
 }
 
-func transformString(s string, from, to int, res string) string {
-	sb := strings.Builder{}
-	for i := 0; i < from; i++ {
-		sb.WriteByte(s[i])
-	}
-	for i := 0; i < len(res); i++ {
-		sb.WriteByte(res[i])
-	}
-	for i := to; i < len(s); i++ {
-		sb.WriteByte(s[i])
-	}
-	return sb.String()
+func magnitude(s string) int {
+	p, _ := parse(s, 1)
+	return p.magnitude()
 }
 
 func parse(s string, i int) (*Pair, int) {
@@ -106,6 +133,22 @@ func parse(s string, i int) (*Pair, int) {
 	return p, i
 }
 
+func (p *Pair) magnitude() int {
+	l := 0
+	r := 0
+	if p.leftPair == nil {
+		l = p.leftNumber
+	} else {
+		l = p.leftPair.magnitude()
+	}
+	if p.rightPair == nil {
+		r = p.rightNumber
+	} else {
+		r = p.rightPair.magnitude()
+	}
+	return 3*l + 2*r
+}
+
 type Pair struct {
 	leftNumber  int
 	leftPair    *Pair
@@ -113,30 +156,19 @@ type Pair struct {
 	rightPair   *Pair
 }
 
-func (p *Pair) String() string {
-	s := "["
-	if p.leftPair == nil {
-		s += strconv.Itoa(p.leftNumber)
-	} else {
-		s += p.leftPair.String()
+func transformString(s string, from, to int, res string) string {
+	sb := strings.Builder{}
+	for i := 0; i < from; i++ {
+		sb.WriteByte(s[i])
 	}
-	s += ","
-	if p.rightPair == nil {
-		s += strconv.Itoa(p.rightNumber)
-	} else {
-		s += p.rightPair.String()
+	for i := 0; i < len(res); i++ {
+		sb.WriteByte(res[i])
 	}
-	return s + "]"
+	for i := to; i < len(s); i++ {
+		sb.WriteByte(s[i])
+	}
+	return sb.String()
 }
-
-//func explode(p *Pair) *Pair {
-//	res := ""
-//	s := p.String()
-//	open := 0
-//	for i := 0; i < len(s); i++ {
-//		if s[i] ==
-//	}
-//}
 
 func fs2(input io.Reader) int {
 	scanner := bufio.NewScanner(input)
--- 2021/day18-go/main_test.go
+++ 2021/day18-go/main_test.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestParse(t *testing.T) {
-	v := "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
-	pair, _ := parse(v, 1)
-	assert.Equal(t, v, pair.String())
-}
-
 func TestFs1Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 42, fs1(f))
+	assert.Equal(t, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", fs1(f))
+}
+
+func TestFs1Unit(t *testing.T) {
+	assert.Equal(t, "[[[[5,0],[7,4]],[5,5]],[6,6]]", fs1(strings.NewReader(`[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]`)))
 }
 
 func TestFs1Input(t *testing.T) {
@@ -39,18 +43,44 @@ func TestFs2Input(t *testing.T) {
 }
 
 func TestExplode(t *testing.T) {
-	assert.Equal(t, "[[[[0,9],2],3],4]", explode("[[[[[9,8],1],2],3],4]"))
-	assert.Equal(t, "[7,[6,[5,[7,0]]]]", explode("[7,[6,[5,[4,[3,2]]]]]"))
-	assert.Equal(t, "[[6,[5,[7,0]]],3]", explode("[[6,[5,[4,[3,2]]]],1]"))
-	assert.Equal(t, "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"))
-	assert.Equal(t, "[[3,[2,[8,0]]],[9,[5,[7,0]]]]", explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"))
+	testExplode(t, "[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")
+	testExplode(t, "[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")
+	testExplode(t, "[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")
+	testExplode(t, "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+	testExplode(t, "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+	testExplode(t, "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[15,[0,13]]],[1,1]]")
 }
 
-func Test_transformString(t *testing.T) {
+func testExplode(t *testing.T, from, to string) {
+	s, b := explode(from)
+	assert.True(t, b)
+	assert.Equal(t, to, s)
+}
+
+func TestTransformString(t *testing.T) {
 	assert.Equal(t, "01x45", transformString("012345", 2, 4, "x"))
 	assert.Equal(t, "01x345", transformString("012345", 2, 3, "x"))
 }
 
-func Test_add(t *testing.T) {
-	add()
+func TestAdd(t *testing.T) {
+	assert.Equal(t, "[[1,2],[[3,4],5]]", add("[1,2]", "[[3,4],5]"))
+}
+
+func TestSplit(t *testing.T) {
+	s, b := split("[[[[0,7],4],[15,[0,13]]],[1,1]]")
+	assert.True(t, b)
+	assert.Equal(t, "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", s)
+
+	s2, b2 := split("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")
+	assert.True(t, b2)
+	assert.Equal(t, "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]", s2)
+
+	s2, b2 = split("[9,[19,9]]")
+	assert.True(t, b2)
+	assert.Equal(t, "[9,[[9,10],9]]", s2)
+}
+
+func TestMagnitude(t *testing.T) {
+	//assert.Equal(t, 129, magnitude("[[9,1],[1,9]]"))
+	assert.Equal(t, 3488, magnitude("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"))
 }
