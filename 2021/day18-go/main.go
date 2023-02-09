3afb322657da9efbce1697518559b9b912469dbd
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-02-09 23:16:58 +0100

    Checkpoint

--- 2021/day18-go/main.go
+++ 2021/day18-go/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strconv"
+	"strings"
 
 	aoc "github.com/teivah/advent-of-code"
 )
@@ -19,125 +21,122 @@ func fs1(input io.Reader) int {
 	return 42
 }
 
-func parse(s string, i int) (*Pair, int) {
-	if s[i] == '[' {
-		pair1, j := parse(s, i+1)
-		i = j + 1
-
-		if s[i] == '[' {
-			pair2, j := parse(s, i+1)
-			return &Pair{
-				x: Element{
-					pair: pair1,
-				},
-				y: Element{
-					pair: pair2,
-				},
-			}, j
-		} else {
-			return &Pair{
-				x: Element{
-					pair: pair1,
-				},
-				y: Element{
-					number: aoc.RuneToInt(rune(s[i])),
-				},
-			}, i + 2 // Skip ]
-		}
-	} else {
-		number1 := aoc.RuneToInt(rune(s[i]))
-		i += 2 // Skip ,
-
-		if s[i] == '[' {
-			pair, j := parse(s, i+1)
-			return &Pair{
-				x: Element{
-					number: number1,
-				},
-				y: Element{
-					pair: pair,
-				},
-			}, j
-		} else {
-			return &Pair{
-				x: Element{
-					number: number1,
-				},
-				y: Element{
-					number: aoc.RuneToInt(rune(s[i])),
-				},
-			}, i + 2 // Skip ]
-		}
-	}
+func add(s1, s2 string) string {
+	return fmt.Sprintf("[%s,%s]")
 }
 
-type Element struct {
-	number int
-	pair   *Pair
+func explode(s string) string {
+	res := ""
+
+	open := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '[' {
+			open++
+			if open == 5 {
+				r1 := rune(s[i+1])
+				r2 := rune(s[i+3])
+				if aoc.IsRuneDecimal(r1) && aoc.IsRuneDecimal(r2) {
+					// Explode
+					x := aoc.RuneToInt(r1)
+					y := aoc.RuneToInt(r2)
+					s = transformString(s, i, i+5, "0")
+
+					for left := i - 1; left >= 0; left-- {
+						r := rune(s[left])
+						if aoc.IsRuneDecimal(r) {
+							v := strconv.Itoa(aoc.RuneToInt(r) + x)
+							s = transformString(s, left, left+1, v)
+							break
+						}
+					}
+
+					for right := i + 1; right < len(s); right++ {
+						r := rune(s[right])
+						if aoc.IsRuneDecimal(r) {
+							v := strconv.Itoa(aoc.RuneToInt(r) + y)
+							s = transformString(s, right, right+1, v)
+							break
+						}
+					}
+
+					return s
+				}
+			}
+		} else if s[i] == ']' {
+			open--
+		}
+	}
+
+	return res
+}
+
+func transformString(s string, from, to int, res string) string {
+	sb := strings.Builder{}
+	for i := 0; i < from; i++ {
+		sb.WriteByte(s[i])
+	}
+	for i := 0; i < len(res); i++ {
+		sb.WriteByte(res[i])
+	}
+	for i := to; i < len(s); i++ {
+		sb.WriteByte(s[i])
+	}
+	return sb.String()
+}
+
+func parse(s string, i int) (*Pair, int) {
+	p := &Pair{}
+	if s[i] == '[' {
+		pair, j := parse(s, i+1)
+		p.leftPair = pair
+		i = j + 1
+	} else {
+		p.leftNumber = aoc.RuneToInt(rune(s[i]))
+		i += 2
+	}
+
+	if s[i] == '[' {
+		pair, j := parse(s, i+1)
+		p.rightPair = pair
+		i = j + 1
+	} else {
+		p.rightNumber = aoc.RuneToInt(rune(s[i]))
+		i += 2
+	}
+	return p, i
 }
 
 type Pair struct {
-	x Element
-	y Element
-}
-
-func (e *Element) explode() {
-	e.dfs(0)
-}
-
-func (e *Element) dfs(currentLevel int) (int, bool, int, bool) {
-	if e.pair == nil {
-		return 0, false, 0, false
-	}
-
-	if currentLevel >=4  {
-		var xv int
-		var xe bool
-		if e.pair.x.pair == nil {
-			xv = e.pair.x.number
-			xe = true
-		} else {
-			xv,
-		}
-
-		var yv int
-		var ye bool
-		if e.pair.y.pair == nil {
-			yv = e.pair.y.number
-			ye = true
-		} else {
-
-		}
-
-
-		return e.pair.x., true, e.pair.y, true
-	}
-
-	if e.pair.x.pair != nil {
-		x, y, exploded := e.pair.x.dfs(currentLevel + 1)
-		if exploded {
-			if e.number {
-
-			}
-		}
-	}
+	leftNumber  int
+	leftPair    *Pair
+	rightNumber int
+	rightPair   *Pair
 }
 
 func (p *Pair) String() string {
 	s := "["
-	if p.x.pair == nil {
-		s += strconv.Itoa(p.x.number)
+	if p.leftPair == nil {
+		s += strconv.Itoa(p.leftNumber)
 	} else {
-		s += p.x.pair.String()
+		s += p.leftPair.String()
 	}
 	s += ","
-	if p.y.pair == nil {
-		s += strconv.Itoa(p.y.number)
+	if p.rightPair == nil {
+		s += strconv.Itoa(p.rightNumber)
 	} else {
-		s += p.y.pair.String()
+		s += p.rightPair.String()
 	}
 	return s + "]"
 }
+
+//func explode(p *Pair) *Pair {
+//	res := ""
+//	s := p.String()
+//	open := 0
+//	for i := 0; i < len(s); i++ {
+//		if s[i] ==
+//	}
+//}
 
 func fs2(input io.Reader) int {
 	scanner := bufio.NewScanner(input)
--- 2021/day18-go/main_test.go
+++ 2021/day18-go/main_test.go
@@ -8,14 +8,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestExplode(t *testing.T) {
-	testExplode(t, "[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")
-}
-
-func testExplode(t *testing.T, from, to string) {
-	pair, _ := parse(from, 1)
-	pair.explode()
-	assert.Equal(t, to, pair.String())
+func TestParse(t *testing.T) {
+	v := "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
+	pair, _ := parse(v, 1)
+	assert.Equal(t, v, pair.String())
 }
 
 func TestFs1Test(t *testing.T) {
@@ -40,4 +36,21 @@ func TestFs2Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
 	assert.Equal(t, 42, fs2(f))
+}
+
+func TestExplode(t *testing.T) {
+	assert.Equal(t, "[[[[0,9],2],3],4]", explode("[[[[[9,8],1],2],3],4]"))
+	assert.Equal(t, "[7,[6,[5,[7,0]]]]", explode("[7,[6,[5,[4,[3,2]]]]]"))
+	assert.Equal(t, "[[6,[5,[7,0]]],3]", explode("[[6,[5,[4,[3,2]]]],1]"))
+	assert.Equal(t, "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"))
+	assert.Equal(t, "[[3,[2,[8,0]]],[9,[5,[7,0]]]]", explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"))
+}
+
+func Test_transformString(t *testing.T) {
+	assert.Equal(t, "01x45", transformString("012345", 2, 4, "x"))
+	assert.Equal(t, "01x345", transformString("012345", 2, 3, "x"))
+}
+
+func Test_add(t *testing.T) {
+	add()
 }
