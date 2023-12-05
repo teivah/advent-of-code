7acb49bc783d4399761067778c64990ea72511d9
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-05 18:40:14 +0100

    Day 5: better algorithm

---
+++ 2023/day5-go/README.md
@@ -0,0 +1,18 @@
+# Day 5
+
+## Part 1
+
+Part 1 is straightforward (see `fs1` function).
+
+## Part 2
+
+At first, I came up with a brute force solution to go over each range of seeds. I leveraged concurrency to get a somewhat decent performance: ~14s (see `fs2NonFinal` function).
+
+Then, I added a counter to see the range of numbers I was checking. I realized this number was about 10 times bigger than the final solution. Therefore, I came up with the reverse solution (see `fs2` function):
+
+- Iterating from 0 to `math.MaxInt` for the location variable
+- Going reverse (from location to humidity, etc.) until I get the seed value
+- Then, I check whether the seed is among the provided ranges using a binary search
+- If yes, I return the location
+
+This version takes about 1.75s to complete.
--- 2023/day5-go/main.go
+++ 2023/day5-go/main.go
@@ -94,7 +94,8 @@ func parseMap(lines []string) Map {
 	return Map{ranges: ranges}
 }
 
-func fs2(input io.Reader) int {
+// See fs2 for the final version
+func fs2NonFinal(input io.Reader) int {
 	groups := aoc.StringGroups(aoc.ReaderToStrings(input))
 
 	seeds := parseSeedsRange(groups[0][0])
@@ -142,4 +143,79 @@ func parseSeedsRange(line string) [][2]int {
 		res = append(res, [2]int{ints[i], ints[i+1]})
 	}
 	return res
+}
+
+func fs2(input io.Reader) int {
+	groups := aoc.StringGroups(aoc.ReaderToStrings(input))
+
+	seeds := parseSeedsRange(groups[0][0])
+	sort.Slice(seeds, func(i, j int) bool {
+		return seeds[i][0] < seeds[j][0]
+	})
+	var maps []Map
+	for i := 1; i < len(groups); i++ {
+		maps = append(maps, parseRevMap(groups[i]))
+	}
+
+	for loc := 0; loc < math.MaxInt; loc++ {
+		v := transformRev(loc, maps)
+		if isWithinSeedRange(v, seeds) {
+			return loc
+		}
+	}
+
+	return -1
+}
+
+func transformRev(v int, maps []Map) int {
+	for i := len(maps) - 1; i >= 0; i-- {
+		m := maps[i]
+		if dst, contains := m.get(v); contains {
+			v = dst
+		}
+	}
+	return v
+}
+
+func parseRevMap(lines []string) Map {
+	var ranges []Range
+	for i := 0; i < len(lines); i++ {
+		if i == 0 {
+			// Discard header
+			continue
+		}
+		del := aoc.NewDelimiter(lines[i], " ")
+		ints := del.GetInts()
+		dstRange := ints[0]
+		srcRange := ints[1]
+		rangeLength := ints[2]
+
+		ranges = append(ranges, Range{
+			from:    dstRange,
+			to:      dstRange + rangeLength - 1,
+			transfo: srcRange - dstRange,
+		})
+	}
+	sort.Slice(ranges, func(i, j int) bool {
+		return ranges[i].from < ranges[j].from
+	})
+	return Map{ranges: ranges}
+}
+
+func isWithinSeedRange(n int, seeds [][2]int) bool {
+	l := 0
+	r := len(seeds) - 1
+	for l <= r {
+		mid := l + (r-l)/2
+		rng := seeds[mid]
+		if n < rng[0] {
+			r = mid - 1
+		} else if n > rng[0]+rng[1]-1 {
+			l = mid + 1
+		} else {
+			return true
+		}
+	}
+
+	return false
 }
