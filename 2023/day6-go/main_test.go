498ac56a804bc1f3f29612b1cecf4bb86d25ba99
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-06 09:24:55 +0100

    Day 6

---
+++ 2023/day6-go/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFs1Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 288, fs1(f))
+}
+
+func TestFs1Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 608902, fs1(f))
+}
+
+func TestFs2Test(t *testing.T) {
+	f, err := os.Open("test.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 71503, fs2(f))
+}
+
+func TestFs2Input(t *testing.T) {
+	f, err := os.Open("input.txt")
+	require.NoError(t, err)
+	assert.Equal(t, 46173809, fs2(f))
+}
+
+func Test_travelDistance(t *testing.T) {
+	assert.Equal(t, 0, travelDistance(0, 7))
+	assert.Equal(t, 0, travelDistance(7, 7))
+	assert.Equal(t, 12, travelDistance(3, 7))
+}
