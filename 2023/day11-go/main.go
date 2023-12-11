a8721d3a1a6ede4ad5d8018c59f589e83be24b63
teivah <teiva.harsanyi@gmail.com>
2023-12-11 18:08:08 +0100

    README

--- 2023/day11-go/main.go
+++ 2023/day11-go/main.go
@@ -6,42 +6,46 @@ import (
 	aoc "github.com/teivah/advent-of-code"
 )
 
-func fs1(input io.Reader) int {
+func fs(input io.Reader, expansion int) int {
 	lines := aoc.ReaderToStrings(input)
-	board, rows, cols := parse(lines)
-
-	var positions []aoc.Position
-	for pos := range board {
-		positions = append(positions, pos)
-	}
+	positions, rowsDistance, colsDistance := parse(lines, expansion)
 
 	res := 0
 	for i := 0; i < len(positions); i++ {
 		for j := i + 1; j < len(positions); j++ {
-			res += getDistance(rows, cols, positions[i], positions[j], 1)
+			pos1 := positions[i]
+			pos2 := positions[j]
+			res += aoc.Abs(rowsDistance[pos1.Row]-rowsDistance[pos2.Row]) +
+				aoc.Abs(colsDistance[pos1.Col]-colsDistance[pos2.Col])
 		}
 	}
 	return res
 }
 
-func parse(lines []string) (map[aoc.Position]bool, map[int]bool, map[int]bool) {
-	m := make(map[aoc.Position]bool)
-	rows := make(map[int]bool)
+func parse(lines []string, expansion int) ([]aoc.Position, []int, []int) {
+	var positions []aoc.Position
+	rowsDistance := make([]int, len(lines))
+	sum := 0
 	for row, line := range lines {
 		runes := []rune(line)
 		empty := true
 		for col, r := range runes {
 			if r == '#' {
-				m[aoc.Position{Row: row, Col: col}] = true
+				positions = append(positions, aoc.Position{Row: row, Col: col})
 				empty = false
 			}
 		}
+
+		rowsDistance[row] = sum
 		if empty {
-			rows[row] = true
+			sum += expansion
+		} else {
+			sum++
 		}
 	}
 
-	cols := make(map[int]bool)
+	colsDistance := make([]int, len(lines[0]))
+	sum = 0
 	for col := 0; col < len(lines[0]); col++ {
 		empty := true
 		for row := 0; row < len(lines); row++ {
@@ -50,62 +54,14 @@ func parse(lines []string) (map[aoc.Position]bool, map[int]bool, map[int]bool) {
 				break
 			}
 		}
+
+		colsDistance[col] = sum
 		if empty {
-			cols[col] = true
+			sum += expansion
+		} else {
+			sum++
 		}
 	}
 
-	return m, rows, cols
-}
-
-func getDistance(rows map[int]bool, cols map[int]bool, pos1, pos2 aoc.Position, empty int) int {
-	d := pos1.Manhattan(pos2)
-
-	if pos1.Row < pos2.Row {
-		for row := pos1.Row; row < pos2.Row; row++ {
-			if rows[row] {
-				d += empty
-			}
-		}
-	} else if pos1.Row > pos2.Row {
-		for row := pos1.Row - 1; row > pos2.Row; row-- {
-			if rows[row] {
-				d += empty
-			}
-		}
-	}
-
-	if pos1.Col < pos2.Col {
-		for Col := pos1.Col; Col < pos2.Col; Col++ {
-			if cols[Col] {
-				d += empty
-			}
-		}
-	} else if pos1.Col > pos2.Col {
-		for Col := pos1.Col - 1; Col > pos2.Col; Col-- {
-			if cols[Col] {
-				d += empty
-			}
-		}
-	}
-
-	return d
-}
-
-func fs2(input io.Reader, empty int) int {
-	lines := aoc.ReaderToStrings(input)
-	board, rows, cols := parse(lines)
-
-	var positions []aoc.Position
-	for pos := range board {
-		positions = append(positions, pos)
-	}
-
-	res := 0
-	for i := 0; i < len(positions); i++ {
-		for j := i + 1; j < len(positions); j++ {
-			res += getDistance(rows, cols, positions[i], positions[j], empty-1)
-		}
-	}
-	return res
+	return positions, rowsDistance, colsDistance
 }
--- 2023/day11-go/main_test.go
+++ 2023/day11-go/main_test.go
@@ -11,30 +11,30 @@ import (
 func TestFs1Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 374, fs1(f))
+	assert.Equal(t, 374, fs(f, 2))
 }
 
 func TestFs1Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 10885634, fs1(f))
+	assert.Equal(t, 10885634, fs(f, 2))
 }
 
 func TestFs2Test(t *testing.T) {
 	t.Run("10", func(t *testing.T) {
 		f, err := os.Open("test.txt")
 		require.NoError(t, err)
-		assert.Equal(t, 1030, fs2(f, 10))
+		assert.Equal(t, 1030, fs(f, 10))
 	})
 	t.Run("100", func(t *testing.T) {
 		f, err := os.Open("test.txt")
 		require.NoError(t, err)
-		assert.Equal(t, 8410, fs2(f, 100))
+		assert.Equal(t, 8410, fs(f, 100))
 	})
 }
 
 func TestFs2Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 1131, fs2(f, 1000000))
+	assert.Equal(t, 707505470642, fs(f, 1000000))
 }
