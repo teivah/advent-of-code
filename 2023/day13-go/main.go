7f1e5f3dd4249f6c9711588eb2d1aad067723c92
teivah <teiva.harsanyi@gmail.com>
2023-12-13 10:09:12 +0100

    Day 13

--- 2023/day13-go/main.go
+++ 2023/day13-go/main.go
@@ -7,16 +7,16 @@ import (
 	aoc "github.com/teivah/advent-of-code"
 )
 
-func fs1(input io.Reader) int {
+func fs(input io.Reader, maxSmudge int) int {
 	groups := aoc.StringGroups(aoc.ReaderToStrings(input))
 
 	res := 0
 	for _, group := range groups {
-		if v, found := findHorizontal(group); found {
+		if v, found := findHorizontal(group, maxSmudge); found {
 			res += 100 * v
 			continue
 		}
-		if v, found := findVertical(group); found {
+		if v, found := findVertical(group, maxSmudge); found {
 			res += v
 			continue
 		}
@@ -26,17 +26,21 @@ func fs1(input io.Reader) int {
 	return res
 }
 
-func findHorizontal(lines []string) (int, bool) {
-	numbers := make([]int, 0, len(lines))
+func findHorizontal(lines []string, maxSmudge int) (int, bool) {
+	hashes := make([]int, 0, len(lines))
+	variations := make([][]int, 0, len(lines))
 	for _, line := range lines {
-		numbers = append(numbers, stringToNumber(line))
+		h, v := getHashAndVariations(line)
+		hashes = append(hashes, h)
+		variations = append(variations, v)
 	}
 
-	return find(numbers)
+	return find(hashes, variations, len(lines[0]), maxSmudge)
 }
 
-func findVertical(lines []string) (int, bool) {
-	numbers := make([]int, 0, len(lines))
+func findVertical(lines []string, maxSmudge int) (int, bool) {
+	hashes := make([]int, 0, len(lines))
+	variations := make([][]int, 0, len(lines))
 
 	for col := 0; col < len(lines[0]); col++ {
 		sb := strings.Builder{}
@@ -44,62 +48,15 @@ func findVertical(lines []string) (int, bool) {
 		for row := 0; row < len(lines); row++ {
 			sb.WriteRune(rune(lines[row][col]))
 		}
-		numbers = append(numbers, stringToNumber(sb.String()))
+		h, v := getHashAndVariations(sb.String())
+		hashes = append(hashes, h)
+		variations = append(variations, v)
 	}
 
-	return find(numbers)
+	return find(hashes, variations, len(lines), maxSmudge)
 }
 
-func find(numbers []int) (int, bool) {
-	for i := 1; i < len(numbers); i++ {
-		found := true
-		for idx1, idx2 := i-1, i; ; idx1, idx2 = idx1-1, idx2+1 {
-			if idx1 < 0 || idx2 >= len(numbers) {
-				break
-			}
-			if numbers[idx1] != numbers[idx2] {
-				found = false
-				break
-			}
-		}
-		if found {
-			return i, true
-		}
-	}
-
-	return 0, false
-}
-
-func stringToNumber(s string) int {
-	res := 0
-	for i := 0; i < len(s); i++ {
-		if s[i] == '#' {
-			res += 1 << i
-		}
-	}
-	return res
-}
-
-func fs2(input io.Reader) int {
-	groups := aoc.StringGroups(aoc.ReaderToStrings(input))
-
-	res := 0
-	for _, group := range groups {
-		if v, found := findHorizontalWithSmudge(group); found {
-			res += 100 * v
-			continue
-		}
-		if v, found := findVerticalWithSmudge(group); found {
-			res += v
-			continue
-		}
-		panic(group)
-	}
-
-	return res
-}
-
-func stringToNumberAndVariations(s string) (int, []int) {
+func getHashAndVariations(s string) (int, []int) {
 	res := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == '#' {
@@ -121,66 +78,36 @@ func stringToNumberAndVariations(s string) (int, []int) {
 	return res, variations
 }
 
-func findHorizontalWithSmudge(lines []string) (int, bool) {
-	numbers := make([]int, 0, len(lines))
-	variations := make([][]int, 0, len(lines))
-	for _, line := range lines {
-		n, v := stringToNumberAndVariations(line)
-		numbers = append(numbers, n)
-		variations = append(variations, v)
-	}
-
-	return findWithSmudge(numbers, variations, len(lines[0]))
-}
-
-func findVerticalWithSmudge(lines []string) (int, bool) {
-	numbers := make([]int, 0, len(lines))
-	variations := make([][]int, 0, len(lines))
-
-	for col := 0; col < len(lines[0]); col++ {
-		sb := strings.Builder{}
-		sb.Grow(len(lines[0]))
-		for row := 0; row < len(lines); row++ {
-			sb.WriteRune(rune(lines[row][col]))
-		}
-		n, v := stringToNumberAndVariations(sb.String())
-		numbers = append(numbers, n)
-		variations = append(variations, v)
-	}
-
-	return findWithSmudge(numbers, variations, len(lines))
-}
-
-func findWithSmudge(numbers []int, variations [][]int, max int) (int, bool) {
-	for i := 1; i < len(numbers); i++ {
+func find(hashes []int, variations [][]int, max int, maxSmudge int) (int, bool) {
+outer:
+	for i := 1; i < len(hashes); i++ {
 		smudge := 0
 		for idx1, idx2 := i-1, i; ; idx1, idx2 = idx1-1, idx2+1 {
-			if idx1 < 0 || idx2 >= len(numbers) {
+			if idx1 < 0 || idx2 >= len(hashes) {
 				break
 			}
-			if numbers[idx1] == numbers[idx2] {
+			if hashes[idx1] == hashes[idx2] {
 				continue
 			}
 
-			if smudge == 1 {
-				smudge = -1
-				break
+			if smudge == maxSmudge {
+				continue outer
 			}
 
+			// If the hashes do not match, we check the variations.
 			found := false
-			for col := 0; col < max; col++ {
-				if numbers[idx1] == variations[idx2][col] || variations[idx1][col] == numbers[idx2] {
+			for j := 0; j < max; j++ {
+				if hashes[idx1] == variations[idx2][j] || variations[idx1][j] == hashes[idx2] {
 					smudge++
 					found = true
 					break
 				}
 			}
 			if !found {
-				smudge = -1
-				break
+				continue outer
 			}
 		}
-		if smudge == 1 {
+		if smudge == maxSmudge {
 			return i, true
 		}
 	}
--- 2023/day13-go/main_test.go
+++ 2023/day13-go/main_test.go
@@ -11,23 +11,23 @@ import (
 func TestFs1Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 405, fs1(f))
+	assert.Equal(t, 405, fs(f, 0))
 }
 
 func TestFs1Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 41859, fs1(f))
+	assert.Equal(t, 41859, fs(f, 0))
 }
 
 func TestFs2Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 400, fs2(f))
+	assert.Equal(t, 400, fs(f, 1))
 }
 
 func TestFs2Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
-	assert.Equal(t, 30842, fs2(f))
+	assert.Equal(t, 30842, fs(f, 1))
 }
