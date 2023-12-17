2b4245c32285f18a13008bf2094e24fa5ce436c0
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-17 11:16:57 +0100

    Update ParseBoard

--- lib_board.go
+++ lib_board.go
@@ -3,14 +3,14 @@ package aoc
 import "io"
 
 // ParseBoard parses a board and maps it to a map of Position.
-func ParseBoard[T any](input io.Reader, fn func(r rune) T) map[Position]T {
+func ParseBoard[T any](input io.Reader, fn func(r rune) T) (board map[Position]T, rows int, cols int) {
 	lines := ReaderToStrings(input)
-	board := make(map[Position]T, len(lines)*len(lines[0]))
+	board = make(map[Position]T, len(lines)*len(lines[0]))
 	for row, line := range lines {
 		runes := []rune(line)
 		for col, c := range runes {
 			board[NewPosition(row, col)] = fn(c)
 		}
 	}
-	return board
+	return board, len(lines), len(lines[0])
 }
--- lib_board_test.go
+++ lib_board_test.go
@@ -10,11 +10,13 @@ import (
 func TestParseBoard(t *testing.T) {
 	reader := strings.NewReader(`01
 23`)
-	board := ParseBoard[int](reader, func(r rune) int {
+	board, rows, cols := ParseBoard[int](reader, func(r rune) int {
 		return int(r - '0')
 	})
 	assert.Equal(t, 0, board[NewPosition(0, 0)])
 	assert.Equal(t, 1, board[NewPosition(0, 1)])
 	assert.Equal(t, 2, board[NewPosition(1, 0)])
 	assert.Equal(t, 3, board[NewPosition(1, 1)])
+	assert.Equal(t, 2, rows)
+	assert.Equal(t, 2, cols)
 }
