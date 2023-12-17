7920cb7c087798f76151335beaec2e8fa329c796
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-17 11:14:02 +0100

    ParseBoard

---
+++ lib_board.go
@@ -0,0 +1,16 @@
+package aoc
+
+import "io"
+
+// ParseBoard parses a board and maps it to a map of Position.
+func ParseBoard[T any](input io.Reader, fn func(r rune) T) map[Position]T {
+	lines := ReaderToStrings(input)
+	board := make(map[Position]T, len(lines)*len(lines[0]))
+	for row, line := range lines {
+		runes := []rune(line)
+		for col, c := range runes {
+			board[NewPosition(row, col)] = fn(c)
+		}
+	}
+	return board
+}
---
+++ lib_board_test.go
@@ -0,0 +1,20 @@
+package aoc
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseBoard(t *testing.T) {
+	reader := strings.NewReader(`01
+23`)
+	board := ParseBoard[int](reader, func(r rune) int {
+		return int(r - '0')
+	})
+	assert.Equal(t, 0, board[NewPosition(0, 0)])
+	assert.Equal(t, 1, board[NewPosition(0, 1)])
+	assert.Equal(t, 2, board[NewPosition(1, 0)])
+	assert.Equal(t, 3, board[NewPosition(1, 1)])
+}
