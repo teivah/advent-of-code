ad386b3e87ef753a29fba84eb2b29462ffcbdc73
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-18 12:17:12 +0100

    Day 18: part 1

--- 2023/day18-go/main.go
+++ 2023/day18-go/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"math"
+	"strconv"
 
 	aoc "github.com/teivah/advent-of-code"
 )
@@ -139,27 +141,151 @@ func toBoard(input io.Reader) (aoc.Board[Terrain], int, int) {
 }
 
 func fill(board aoc.Board[Terrain], pos aoc.Position, minRows, minCols int) {
-	if pos.Row < minRows || pos.Row >= board.Rows || pos.Col < minCols || pos.Col >= board.Cols {
-		return
-	}
+	q := []aoc.Position{pos}
+	for len(q) != 0 {
+		p := q[0]
+		q = q[1:]
+		if p.Row < minRows || p.Row >= board.Rows || p.Col < minCols || p.Col >= board.Cols {
+			continue
+		}
+		if _, exists := board.Positions[p]; exists {
+			continue
+		}
 
-	if _, exists := board.Positions[pos]; exists {
-		return
+		board.Positions[p] = Terrain{isGround: true}
+		q = append(q, p.Move(aoc.Left, 1))
+		q = append(q, p.Move(aoc.Right, 1))
+		q = append(q, p.Move(aoc.Up, 1))
+		q = append(q, p.Move(aoc.Down, 1))
 	}
-
-	board.Positions[pos] = Terrain{isGround: true}
-	fill(board, pos.Move(aoc.Left, 1), minRows, minCols)
-	fill(board, pos.Move(aoc.Right, 1), minRows, minCols)
-	fill(board, pos.Move(aoc.Up, 1), minRows, minCols)
-	fill(board, pos.Move(aoc.Down, 1), minRows, minCols)
 }
 
 func fs2(input io.Reader) int {
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		_ = line
+	board, minRows, minCols := toBoard2(input)
+
+	for row := minRows; row < board.Rows; row++ {
+		for col := minCols; col < board.Cols; col++ {
+			// Expand right
+			trench := false
+			for c := col + 1; c < board.Cols; c++ {
+				pos := aoc.Position{Row: row, Col: c}
+				if _, exists := board.Positions[pos]; exists {
+					trench = true
+					break
+				}
+			}
+
+			if trench {
+				// Expand Left
+				trench = false
+				for c := col - 1; c >= minCols; c-- {
+					pos := aoc.Position{Row: row, Col: c}
+					if _, exists := board.Positions[pos]; exists {
+						trench = true
+						break
+					}
+				}
+			}
+
+			if trench {
+				// Expand Up
+				trench = false
+				for r := row - 1; r >= minRows; r-- {
+					pos := aoc.Position{Row: r, Col: col}
+					if _, exists := board.Positions[pos]; exists {
+						trench = true
+						break
+					}
+				}
+			}
+
+			if trench {
+				// Expand Down
+				trench = false
+				for r := row + 1; r < board.Rows; r++ {
+					pos := aoc.Position{Row: r, Col: col}
+					if _, exists := board.Positions[pos]; exists {
+						trench = true
+						break
+					}
+				}
+			}
+
+			if trench {
+				continue
+			}
+			fill(board, aoc.Position{Row: row, Col: col}, minRows, minCols)
+		}
 	}
 
-	return 42
+	countTerrain := 0
+	for row := minRows; row < board.Rows; row++ {
+		for col := minCols; col < board.Cols; col++ {
+			if t, exists := board.Positions[aoc.Position{Row: row, Col: col}]; exists {
+				if t.isGround {
+					countTerrain++
+				}
+			}
+		}
+	}
+
+	return (board.Rows-minRows)*(board.Cols-minCols) - countTerrain
+}
+
+func toBoard2(input io.Reader) (aoc.Board[Terrain], int, int) {
+	board := aoc.Board[Terrain]{
+		Positions: make(map[aoc.Position]Terrain),
+	}
+	pos := aoc.Position{}
+	maxRows := 0
+	maxCols := 0
+	minRows := math.MaxInt
+	minCols := math.MaxInt
+
+	scanner := bufio.NewScanner(input)
+	idx := -1
+	for scanner.Scan() {
+		idx++
+		line := scanner.Text()
+		del := aoc.NewDelimiter(line, " ")
+		s := del.GetString(2)
+		color := s[2 : len(s)-1]
+
+		distance := color[:5]
+		v, err := strconv.ParseInt(distance, 16, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		var dir aoc.Direction
+		switch color[5] {
+		case '0':
+			dir = aoc.Right
+		case '1':
+			dir = aoc.Down
+		case '2':
+			dir = aoc.Left
+		case '3':
+			dir = aoc.Up
+		default:
+			panic(color)
+		}
+
+		for i := 0; i < int(v); i++ {
+			pos = pos.Move(dir, 1)
+			board.Positions[pos] = Terrain{}
+		}
+
+		maxRows = max(maxRows, pos.Row)
+		maxCols = max(maxCols, pos.Col)
+		minRows = min(minRows, pos.Row)
+		minCols = min(minCols, pos.Col)
+		board.Positions[pos] = Terrain{}
+	}
+
+	board.Rows = maxRows + 1
+	board.Cols = maxCols + 1
+
+	fmt.Println("parse")
+	return board, minRows, minCols
 }
