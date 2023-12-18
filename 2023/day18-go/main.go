4ed7a507ea770aefa2deb51258f0a183cd7cb791
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-18 18:02:05 +0100

    Day 18: part 2

--- 2023/day18-go/main.go
+++ 2023/day18-go/main.go
@@ -8,146 +8,86 @@ import (
 	aoc "github.com/teivah/advent-of-code"
 )
 
-type Terrain struct {
-	isGround bool
-}
-
 func fs1(input io.Reader) int {
-	board := parse1(input)
-
-	for row := board.MinRows; row < board.MaxRows; row++ {
-		for col := board.MinCols; col < board.MaxCols; col++ {
-			// Expand right
-			trench := false
-			for c := col + 1; c < board.MaxCols; c++ {
-				pos := aoc.Position{Row: row, Col: c}
-				if _, exists := board.Positions[pos]; exists {
-					trench = true
-					break
-				}
-			}
-
-			if trench {
-				// Expand Left
-				trench = false
-				for c := col - 1; c >= board.MinCols; c-- {
-					pos := aoc.Position{Row: row, Col: c}
-					if _, exists := board.Positions[pos]; exists {
-						trench = true
-						break
-					}
-				}
-			}
-
-			if trench {
-				// Expand Up
-				trench = false
-				for r := row - 1; r >= board.MinRows; r-- {
-					pos := aoc.Position{Row: r, Col: col}
-					if _, exists := board.Positions[pos]; exists {
-						trench = true
-						break
-					}
-				}
-			}
-
-			if trench {
-				// Expand Down
-				trench = false
-				for r := row + 1; r < board.MaxRows; r++ {
-					pos := aoc.Position{Row: r, Col: col}
-					if _, exists := board.Positions[pos]; exists {
-						trench = true
-						break
-					}
-				}
-			}
-
-			if trench {
-				continue
-			}
-			fill(board, aoc.Position{Row: row, Col: col})
-		}
-	}
-
-	countTerrain := 0
-	for row := board.MinRows; row < board.MaxRows; row++ {
-		for col := board.MinCols; col < board.MaxCols; col++ {
-			if t, exists := board.Positions[aoc.Position{Row: row, Col: col}]; exists {
-				if t.isGround {
-					countTerrain++
-				}
-			}
-		}
-	}
-
-	return (board.MaxRows-board.MinRows)*(board.MaxCols-board.MinCols) - countTerrain
-}
-
-func parse1(input io.Reader) aoc.Board[Terrain] {
-	positions := make(map[aoc.Position]Terrain)
-	pos := aoc.Position{}
-
-	scanner := bufio.NewScanner(input)
-	idx := -1
-	for scanner.Scan() {
-		idx++
-		line := scanner.Text()
-		del := aoc.NewDelimiter(line, " ")
-		direction := del.GetString(0)
-		count := del.GetInt(1)
-
-		var dir aoc.Direction
-		switch direction {
-		case "R":
-			dir = aoc.Right
-		case "L":
-			dir = aoc.Left
-		case "U":
-			dir = aoc.Up
-		case "D":
-			dir = aoc.Down
-		default:
-			panic(direction)
-		}
-
-		for i := 0; i < count; i++ {
-			pos = pos.Move(dir, 1)
-			positions[pos] = Terrain{}
-		}
-	}
-
-	return aoc.NewBoard(positions)
-}
-
-func fill(board aoc.Board[Terrain], pos aoc.Position) {
-	if _, exists := board.Positions[pos]; exists {
-		return
-	}
-
-	q := []aoc.Position{pos}
-	for len(q) != 0 {
-		p := q[0]
-		q = q[1:]
-		if p.Row < board.MinRows || p.Row >= board.MaxRows || p.Col < board.MinCols || p.Col >= board.MaxCols {
-			continue
-		}
-		if _, exists := board.Positions[p]; exists {
-			continue
-		}
-
-		board.Positions[p] = Terrain{isGround: true}
-		q = append(q, p.Move(aoc.Left, 1))
-		q = append(q, p.Move(aoc.Right, 1))
-		q = append(q, p.Move(aoc.Up, 1))
-		q = append(q, p.Move(aoc.Down, 1))
-	}
+	edges, positions := parse(input, parseFromInstructions)
+	return calculateArea(edges, positions)
 }
 
 func fs2(input io.Reader) int {
-	count, positions := parse2(input)
+	edges, positions := parse(input, parseFromColor)
+	return calculateArea(edges, positions)
+}
 
-	// Source: https://stackoverflow.com/a/717367 (EDIT)
+func parse(input io.Reader, parseFunc func(string) (aoc.Direction, int)) (int, []aoc.Position) {
+	var positions []aoc.Position
+	pos := aoc.Position{}
+	edges := 0
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		dir, count := parseFunc(line)
+
+		pos = pos.Move(dir, count)
+		edges += count
+		positions = append(positions, pos)
+	}
+
+	return edges, positions
+}
+
+func parseFromInstructions(line string) (aoc.Direction, int) {
+	del := aoc.NewDelimiter(line, " ")
+	direction := del.GetString(0)
+	count := del.GetInt(1)
+
+	var dir aoc.Direction
+	switch direction {
+	case "R":
+		dir = aoc.Right
+	case "D":
+		dir = aoc.Down
+	case "L":
+		dir = aoc.Left
+	case "U":
+		dir = aoc.Up
+	default:
+		panic(direction)
+	}
+
+	return dir, count
+}
+
+func parseFromColor(line string) (aoc.Direction, int) {
+	del := aoc.NewDelimiter(line, " ")
+	s := del.GetString(2)
+	color := s[2 : len(s)-1]
+
+	distance := color[:5]
+	count, err := strconv.ParseInt(distance, 16, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	var dir aoc.Direction
+	switch color[5] {
+	case '0':
+		dir = aoc.Right
+	case '1':
+		dir = aoc.Down
+	case '2':
+		dir = aoc.Left
+	case '3':
+		dir = aoc.Up
+	default:
+		panic(color)
+	}
+
+	return dir, int(count)
+}
+
+func calculateArea(edges int, positions []aoc.Position) int {
+	// Source: https://stackoverflow.com/a/717367 (EDIT section)
 	n := len(positions)
 	positions = append(positions, aoc.Position{Row: positions[0].Row, Col: positions[0].Col})
 	positions = append(positions, aoc.Position{Row: positions[1].Row, Col: positions[1].Col})
@@ -156,46 +96,5 @@ func fs2(input io.Reader) int {
 		area += positions[i].Col * (positions[i+1].Row - positions[i-1].Row)
 	}
 
-	return area/2 + count/2 + 1
-}
-
-func parse2(input io.Reader) (int, []aoc.Position) {
-	var positions []aoc.Position
-	pos := aoc.Position{}
-	edges := 0
-
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		del := aoc.NewDelimiter(line, " ")
-		s := del.GetString(2)
-		color := s[2 : len(s)-1]
-
-		distance := color[:5]
-		v, err := strconv.ParseInt(distance, 16, 64)
-		if err != nil {
-			panic(err)
-		}
-
-		var dir aoc.Direction
-		switch color[5] {
-		case '0':
-			dir = aoc.Right
-		case '1':
-			dir = aoc.Down
-		case '2':
-			dir = aoc.Left
-		case '3':
-			dir = aoc.Up
-		default:
-			panic(color)
-		}
-
-		pos = pos.Move(dir, int(v))
-		edges += int(v)
-
-		positions = append(positions, pos)
-	}
-
-	return edges, positions
+	return area/2 + edges/2 + 1
 }
