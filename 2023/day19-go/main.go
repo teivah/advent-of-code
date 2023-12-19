48287e613c7b6ca7d7082b10003a0f3b3ef133cb
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-19 15:05:21 +0100

    Day 19: adding meta information

--- 2023/day19-go/main.go
+++ 2023/day19-go/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 
 	aoc "github.com/teivah/advent-of-code"
 )
+
+const acceptedStr = "A"
+const rejectedStr = "R"
 
 const startingWorkflow = "in"
 
@@ -29,11 +33,12 @@ const (
 type Rating map[string]int
 
 type Step struct {
-	f        stepFunc
-	res      Result
-	variable string
-	cond     Condition
-	value    int
+	f            stepFunc
+	res          Result
+	condVariable string
+	cond         Condition
+	condValue    int
+	next         string
 }
 
 type stepFunc func(Rating) (bool, Result, string)
@@ -117,6 +122,7 @@ func parseWorkflowStep(s string) Step {
 			},
 			res:  res,
 			cond: alwaysTrue,
+			next: next,
 		}
 	}
 
@@ -137,9 +143,9 @@ func parseWorkflowStep(s string) Step {
 	value := aoc.StringToInt(beforeThen[operatorIdx+1:])
 
 	switch next {
-	case "A":
+	case acceptedStr:
 		res = accepted
-	case "R":
+	case rejectedStr:
 		res = rejected
 	default:
 		res = sent
@@ -153,10 +159,11 @@ func parseWorkflowStep(s string) Step {
 				}
 				return false, res, ""
 			},
-			res:      res,
-			variable: variable,
-			cond:     smaller,
-			value:    value,
+			res:          res,
+			condVariable: variable,
+			cond:         smaller,
+			condValue:    value,
+			next:         next,
 		}
 	}
 	return Step{
@@ -166,10 +173,11 @@ func parseWorkflowStep(s string) Step {
 			}
 			return false, 0, ""
 		},
-		res:      res,
-		variable: variable,
-		cond:     greater,
-		value:    value,
+		res:          res,
+		condVariable: variable,
+		cond:         greater,
+		condValue:    value,
+		next:         next,
 	}
 }
 
@@ -196,9 +204,472 @@ func parseRating(s string) (string, int) {
 }
 
 func fs2(input io.Reader) int {
-	//groups := aoc.StringGroups(aoc.ReaderToStrings(input))
-	//workflows := parseWorkflows(groups[0])
-	//ratings := parseRatings(groups[1])
+	groups := aoc.StringGroups(aoc.ReaderToStrings(input))
+	workflows := parseWorkflows(groups[0])
+	_ = workflows
 
-	return 42
+	parent := dependency(workflows, startingWorkflow)
+	defaultRange := Range{
+		from: 1,
+		to:   4000,
+	}
+	intervals := dfs(parent, RangeRating{
+		"x": defaultRange,
+		"m": defaultRange,
+		"a": defaultRange,
+		"s": defaultRange,
+	})
+
+	//workflow := startingWorkflow
+	//outer:
+	//	for _, interval := range intervals {
+	//		for x := interval["x"].from; x <= interval["x"].to; x++ {
+	//			for m := interval["m"].from; m <= interval["m"].to; m++ {
+	//				for a := interval["a"].from; a <= interval["a"].to; a++ {
+	//					for s := interval["s"].from; s <= interval["s"].to; s++ {
+	//						rating := Rating{
+	//							"x": x,
+	//							"m": m,
+	//							"a": a,
+	//							"s": s,
+	//						}
+	//
+	//					inner:
+	//						for {
+	//							steps := workflows[workflow]
+	//
+	//							for _, step := range steps {
+	//								matching, result, next := step.f(rating)
+	//								if !matching {
+	//									continue
+	//								}
+	//								switch result {
+	//								case rejected:
+	//									panic(fmt.Sprintf("%v", rating))
+	//								case accepted:
+	//									continue outer
+	//								case sent:
+	//									workflow = next
+	//									continue inner
+	//								default:
+	//									panic(result)
+	//								}
+	//							}
+	//						}
+	//					}
+	//				}
+	//			}
+	//		}
+	//	}
+
+	for _, interval := range intervals {
+		fmt.Println(interval)
+	}
+
+	endpoints := make(map[string]map[int]struct{})
+	for _, k := range []string{"x", "m", "a", "s"} {
+		m := make(map[int]struct{})
+		for _, interval := range intervals {
+			r := interval[k]
+			m[r.from] = struct{}{}
+			m[r.to] = struct{}{}
+		}
+		m[1] = struct{}{}
+		m[4000] = struct{}{}
+		endpoints[k] = m
+	}
+
+	ordered := make(map[string][]int)
+	for k, m := range endpoints {
+		var s []int
+		for k := range m {
+			s = append(s, k)
+		}
+		sort.Ints(s)
+		ordered[k] = s
+	}
+
+	fmt.Println()
+	fmt.Println("ordered")
+	for k, v := range ordered {
+		fmt.Println(k, v)
+	}
+	fmt.Println()
+
+	var keys []map[string][2]int
+	for _, interval := range intervals {
+		key := make(map[string][2]int)
+		for _, k := range []string{"x", "m", "a", "s"} {
+			r := interval[k]
+			from := r.from
+			to := r.to
+
+			ints := ordered[k]
+			startIdx := 0
+			endIdx := 0
+			for idx, i := range ints {
+				if i == from {
+					startIdx = idx
+				}
+				if i == to {
+					if i == 4000 {
+						endIdx = idx + 1
+					} else {
+						endIdx = idx
+					}
+					break
+				}
+			}
+			key[k] = [2]int{startIdx, endIdx}
+		}
+		keys = append(keys, key)
+	}
+
+	type entry struct {
+		x int
+		m int
+		a int
+		s int
+	}
+
+	//for _, key := range keys {
+	//	fmt.Println(key)
+	//}
+
+	cache := make(map[entry]bool)
+	res := 0
+	for i, key := range keys {
+		fmt.Println(i, len(keys))
+		x := key["x"]
+		m := key["m"]
+		a := key["a"]
+		s := key["s"]
+
+		specialX := false
+		if x[0] == x[1] {
+			specialX = true
+			x[1]++
+		}
+		specialM := false
+		if m[0] == m[1] {
+			specialM = true
+			m[1]++
+		}
+		specialA := false
+		if a[0] == a[1] {
+			specialA = true
+			a[1]++
+		}
+		specialS := false
+		if s[0] == s[1] {
+			specialS = true
+			s[1]++
+		}
+
+		for iX := x[0]; iX < x[1]; iX++ {
+			fromX := ordered["x"][iX]
+			toX := 0
+			if specialX {
+				toX = fromX
+			} else {
+				if iX+1 == len(ordered["x"]) {
+					toX = 4000
+				} else {
+					toX = ordered["x"][iX+1]
+				}
+			}
+
+			for iM := m[0]; iM < m[1]; iM++ {
+				fromM := ordered["m"][iM]
+				toM := 0
+				if specialM {
+					toM = fromM
+				} else {
+					if iM+1 == len(ordered["m"]) {
+						toM = 4000
+					} else {
+						toM = ordered["m"][iM+1]
+					}
+				}
+
+				for iA := a[0]; iA < a[1]; iA++ {
+					fromA := ordered["a"][iA]
+					toA := 0
+					if specialA {
+						toA = fromA
+					} else {
+						if iA+1 == len(ordered["a"]) {
+							toA = 4000
+						} else {
+							toA = ordered["a"][iA+1]
+						}
+					}
+
+					for iS := s[0]; iS < s[1]; iS++ {
+						fromS := ordered["s"][iS]
+						toS := 0
+						if specialS {
+							toS = fromS
+						} else {
+							if iS+1 == len(ordered["s"]) {
+								toS = 4000
+							} else {
+								toS = ordered["s"][iS+1]
+							}
+						}
+
+						e := entry{
+							x: toX,
+							m: toM,
+							a: toA,
+							s: toS,
+						}
+
+						if cache[e] {
+							continue
+						}
+						fmt.Println(e)
+						cache[e] = true
+
+						res += (toX - fromX + 1) * (toM - fromM + 1) * (toA - fromA + 1) * (toS - fromS + 1)
+					}
+				}
+			}
+		}
+	}
+	return res
+
+	// Overlaps
+	//total := 0
+	//for _, interval := range intervals {
+	//	fmt.Println(interval)
+	//	res := 1
+	//	for _, v := range interval {
+	//		res *= v.to - v.from + 1
+	//	}
+	//	total += res
+	//}
+	//
+	//overlap := 0
+	//for i := 0; i < len(intervals); i++ {
+	//	for j := i + 1; j < len(intervals); j++ {
+	//		overlap += getOverlapping(intervals[i], intervals[j])
+	//	}
+	//}
+	// return total - overlap
+
+	// Segment tree
+	//var parents []*SegmentTreeNode
+	//for _, interval := range intervals {
+	//	parents = segmentTree([]string{"x", "m", "a", "s"}, interval, parents)
+	//}
+
+	return 0
+}
+
+type SegmentTreeNode struct {
+	// Included
+	from int
+	// Included
+	to       int
+	children []*SegmentTreeNode
+}
+
+func segmentTree(keys []string, intervals RangeRating, nodes []*SegmentTreeNode) []*SegmentTreeNode {
+	if len(keys) == 0 {
+		return nil
+	}
+
+	interval := intervals[keys[0]]
+	keys = keys[1:]
+
+	if len(nodes) == 0 {
+		node := &SegmentTreeNode{
+			from: interval.from,
+			to:   interval.to,
+		}
+		node.children = append(node.children, segmentTree(keys, intervals, nil)...)
+		return []*SegmentTreeNode{node}
+	}
+	return nil
+}
+
+func getOverlapping(i1, i2 RangeRating) int {
+	// map[a:{0 2005} m:{0 4000} s:{0 1350} x:{0 1415}]
+	// map[a:{0 2005} m:{0 4000} s:{0 1350} x:{2663 4000}]
+	defaultRange := Range{
+		from: 1,
+		to:   4000,
+	}
+	r := RangeRating{
+		"x": defaultRange,
+		"m": defaultRange,
+		"a": defaultRange,
+		"s": defaultRange,
+	}
+	for k, r1 := range i1 {
+		r2 := i2[k]
+
+		if (r1.from <= r2.from && r2.from <= r1.to) ||
+			(r2.from <= r1.from && r1.from <= r2.to) {
+			v := r[k]
+			v.from = max(v.from, r1.from, r2.from)
+			v.to = min(v.to, r1.to, r2.to)
+			r[k] = v
+		} else {
+			return 0
+		}
+	}
+
+	res := 1
+	for _, v := range r {
+		if v.from > v.to {
+			return 0
+		}
+		res *= v.to - v.from + 1
+	}
+	return res
+}
+
+type Node struct {
+	accepted bool
+	name     string
+	children []*Node
+	steps    []Step
+}
+
+type RangeRating map[string]Range
+
+func (r RangeRating) clone() RangeRating {
+	res := make(map[string]Range, len(r))
+	for k, v := range r {
+		res[k] = v
+	}
+	return res
+}
+
+type Range struct {
+	from int
+	to   int
+}
+
+func dfs(node *Node, r RangeRating) []RangeRating {
+	if node.accepted {
+		for _, v := range r {
+			if v.to < v.from {
+				return nil
+			}
+		}
+		return []RangeRating{r}
+	}
+
+	var res []RangeRating
+	for i, child := range node.children {
+		r := r.clone()
+
+		step := node.steps[i]
+		switch step.cond {
+		case alwaysTrue:
+			res = append(res, dfs(child, r)...)
+		case smaller:
+			variable := step.condVariable
+			rr := r[variable]
+			rr.to = step.condValue - 1
+			r[variable] = rr
+			res = append(res, dfs(child, r)...)
+		case greater:
+			variable := step.condVariable
+			rr := r[variable]
+			rr.from = step.condValue + 1
+			r[variable] = rr
+			res = append(res, dfs(child, r)...)
+		}
+	}
+	return res
+}
+
+func dependency(workflows map[string][]Step, workflowName string) *Node {
+	var children []*Node
+	var steps []Step
+	for _, step := range workflows[workflowName] {
+		switch step.res {
+		case accepted:
+			children = append(children, &Node{
+				accepted: true,
+			})
+			steps = append(steps, step)
+			continue
+		case rejected:
+			continue
+		}
+
+		n := dependency(workflows, step.next)
+		if n != nil {
+			children = append(children, n)
+			steps = append(steps, step)
+		}
+	}
+
+	return &Node{
+		name:     workflowName,
+		children: children,
+		steps:    steps,
+	}
+}
+
+func topologicalSort(workflows map[string][]Step) []string {
+	inDegree := make(map[string]int)
+	for workflow := range workflows {
+		inDegree[workflow] = 0
+	}
+
+	for _, steps := range workflows {
+		for _, step := range steps {
+			vertex := ""
+			switch step.res {
+			case accepted:
+				vertex = acceptedStr
+			case rejected:
+				vertex = rejectedStr
+			case sent:
+				vertex = step.next
+			}
+			inDegree[vertex]++
+		}
+	}
+
+	var q []string
+	for vertex, factor := range inDegree {
+		if factor == 0 {
+			q = append(q, vertex)
+		}
+	}
+
+	var res []string
+	for len(q) != 0 {
+		vertex := q[0]
+		q = q[1:]
+		res = append(res, vertex)
+
+		for _, step := range workflows[vertex] {
+			child := ""
+			switch step.res {
+			case accepted:
+				child = acceptedStr
+			case rejected:
+				child = rejectedStr
+			case sent:
+				child = step.next
+			}
+			inDegree[child]--
+			if inDegree[child] == 0 {
+				q = append(q, child)
+			}
+		}
+	}
+
+	if len(res) != len(workflows)+2 { // Adding accepted and rejected
+		panic("invalid")
+	}
+	return res
 }
--- 2023/day19-go/main_test.go
+++ 2023/day19-go/main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 
+	"day2023-19/segmenttree"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -20,6 +23,150 @@ func TestFs1Input(t *testing.T) {
 	assert.Equal(t, 287054, fs1(f))
 }
 
+func TestFs2(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input string
+		resp  int
+	}{
+		{
+			input: `in{x<2:x}
+x{m<2:m}
+m{a<2:a}
+a{s<2:A}
+`,
+			resp: 1,
+		},
+		{
+			input: `in{x<2:x}
+x{m<2:m}
+m{a<2:a}
+a{s<3:A}
+`,
+			resp: 2,
+		},
+		{
+			input: `in{x<2:x}
+x{m<3:m}
+m{a<2:a}
+a{s<3:A}
+`,
+			resp: 4,
+		},
+		{
+			input: `in{x>3999:x}
+x{m>3999:m}
+m{a>3999:a}
+a{s>3999:A}
+`,
+			resp: 1,
+		},
+		{
+			input: `in{x>3999:x}
+x{m>3999:m}
+m{a>3999:a}
+a{s>3998:A}
+`,
+			resp: 2,
+		},
+		{
+			input: `in{x>3990:x}
+x{m>3990:m}
+m{a>3990:a}
+a{s>3990:A}
+`,
+			resp: 10000,
+		},
+		{
+			input: `in{x>3999:x}
+x{m>3999:m}
+m{a>3999:a}
+a{x<3999:A}
+`,
+			resp: 0,
+		},
+		{
+			input: `in{x>3998:x}
+x{m>3999:m}
+m{a>3999:tmp}
+tmp{s>3999:a}
+a{x<4000:A}
+`,
+			resp: 1,
+		},
+		{
+			input: `in{x<11:x,x>3990:x}
+x{m>3999:m}
+m{a>3999:a}
+a{s>3999:A}
+`,
+			resp: 20,
+		},
+		{
+			input: `in{x>3999:a}
+a{m>3999:b}
+b{a>3999:c}
+c{s>3999:d}
+d{e}
+e{A}
+`,
+			resp: 1,
+		},
+		{
+			input: `in{x>3999:a}
+a{m>3999:b}
+b{a>3999:c}
+c{s>3999:d}
+d{e}
+e{R}
+`,
+			resp: 0,
+		},
+		{
+			input: `in{x<2:a1}
+a1{m>3999:b1}
+b1{a>3999:c1}
+c1{s>3999:A}`,
+			resp: 1,
+		},
+		{
+			input: `in{x<2:a1,x>3999:a1}
+a1{m>3999:b1}
+b1{a>3999:c1}
+c1{s>3999:A}`,
+			resp: 2,
+		},
+		{
+			input: `in{x<2:a1,x>3999:a2}
+a1{m>3999:b1}
+b1{a>3999:c1}
+c1{s>3999:A}
+a2{m<2:b2}
+b2{a<2:c2}
+c2{s<2:A}`,
+			resp: 2,
+		},
+		{
+			input: `in{x<3:a1,x>3998:a2}
+a1{m>3998:b1}
+b1{a>3999:c1}
+c1{s>3999:A}
+a2{m<3:b2}
+b2{a<2:c2}
+c2{s<2:A}`,
+			resp: 8,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			assert.Equal(t, tc.resp, fs2(strings.NewReader(tc.input)))
+		})
+	}
+}
+
 func TestFs2Test(t *testing.T) {
 	f, err := os.Open("test.txt")
 	require.NoError(t, err)
@@ -30,4 +177,60 @@ func TestFs2Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
 	assert.Equal(t, 42, fs2(f))
+}
+
+func TestName(t *testing.T) {
+	st := segmenttree.NewSegmentTree([]int{4000, 4000, 4000, 4000})
+	//st.Build(1, 0, 0)
+	st.Update(1, 0, 0, 0, 0, 2000)
+	fmt.Println(st.Query(4, 0, 0, 0, 4))
+	fmt.Println(st.Query(3, 0, 0, 0, 4))
+	fmt.Println(st.Query(2, 0, 0, 0, 4))
+	fmt.Println(st.Query(1, 0, 0, 0, 4))
+
+}
+
+func Test_getOverlapping(t *testing.T) {
+	intervals := []RangeRating{
+		{
+			"x": {10, 11},
+			"m": {10, 10},
+			"a": {10, 10},
+			"s": {10, 10},
+		},
+		{
+			"x": {10, 10},
+			"m": {10, 10},
+			"a": {10, 10},
+			"s": {10, 10},
+		},
+		{
+			"x": {10, 10},
+			"m": {10, 10},
+			"a": {10, 10},
+			"s": {10, 10},
+		},
+	}
+
+	total := 0
+	for _, interval := range intervals {
+		res := 1
+		for _, v := range interval {
+			res *= v.to - v.from + 1
+		}
+		total += res
+	}
+
+	fmt.Println("total", total)
+
+	overlap := 0
+	for i := 0; i < len(intervals); i++ {
+		for j := i + 1; j < len(intervals); j++ {
+			v := getOverlapping(intervals[i], intervals[j])
+			fmt.Println(v)
+			overlap += v
+		}
+	}
+
+	fmt.Println("res:", total-overlap)
 }
