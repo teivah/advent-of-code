a5fef297e79f2f6ea4211589c3e4a2b4dffb0ad8
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-24 15:10:22 +0100

    2015 Day 3 Haskell

--- 2015/day3-haskell/Main.hs
+++ 2015/day3-haskell/Main.hs
@@ -14,12 +14,7 @@ fn1 [] map _ _ = Map.size map
 fn1 (x:xs) map row col = fn1 xs map2 row2 col2
   where
     map2 = Map.insert (row, col) True map
-    (row2, col2) =
-      case x of
-        '^' -> (row + 1, col)
-        'v' -> (row - 1, col)
-        '<' -> (row, col - 1)
-        '>' -> (row, col + 1)
+    (row2, col2) = transform x row col
 
 fn2 [] map _ _ _ _ _ = Map.size map
 fn2 (x:xs) map sRow sCol rRow rCol sTurn =
@@ -31,18 +26,17 @@ fn2 (x:xs) map sRow sCol rRow rCol sTurn =
         else Map.insert (rRow, rCol) True map
     (sRow2, sCol2) =
       if sTurn
-        then case x of
-               '^' -> (sRow + 1, sCol)
-               'v' -> (sRow - 1, sCol)
-               '<' -> (sRow, sCol - 1)
-               '>' -> (sRow, sCol + 1)
+        then transform x sRow sCol
         else (sRow, sCol)
     (rRow2, rCol2) =
       if not sTurn
-        then case x of
-               '^' -> (rRow + 1, rCol)
-               'v' -> (rRow - 1, rCol)
-               '<' -> (rRow, rCol - 1)
-               '>' -> (rRow, rCol + 1)
+        then transform x rRow rCol
         else (rRow, rCol)
     sTurn2 = not sTurn
+
+transform x row col =
+  case x of
+    '^' -> (row + 1, col)
+    'v' -> (row - 1, col)
+    '<' -> (row, col - 1)
+    '>' -> (row, col + 1)
