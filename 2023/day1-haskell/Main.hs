25ce731dfa9aa1c8c1b0e1b2b95967bc3da8c99a
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-25 01:54:24 +0100

    2023 day 1

--- 2023/day1-haskell/Main.hs
+++ 2023/day1-haskell/Main.hs
@@ -6,9 +6,11 @@ main :: IO ()
 main = do
   res <- withFile "input.txt" ReadMode (\handle -> fn1 handle 0)
   print res
+  res <- withFile "input.txt" ReadMode (\handle -> fn2 handle 0)
+  print res
+  let res = firstDigitLetter "xwadaww" False 0 0
+  print res
 
---  res <- withFile "input.txt" ReadMode (\handle -> fn2 handle 0)
---  print res
 fn1 handle sum = do
   eof <- hIsEOF handle
   if eof
@@ -19,7 +21,6 @@ fn1 handle sum = do
       let v2 = firstDigit (reverse line)
       fn1 handle (sum + v1 * 10 + v2)
 
-firstDigit :: String -> Int
 firstDigit [] = error "no result"
 firstDigit (x:xs) =
   if x >= '0' && x <= '9'
@@ -32,18 +33,32 @@ fn2 handle sum = do
     then return sum
     else do
       line <- hGetLine handle
-      let v1 = firstDigitLetter line
-      print v1
-      fn2 handle 0
+      let v = firstDigitLetter line False 0 0
+      fn2 handle (sum + v)
 
---      fn1 handle (sum + v1 * 10 + v2)
-firstDigitLetter :: String -> Int
-firstDigitLetter [] = error "no result"
-firstDigitLetter all@(x:xs) =
-  if x >= '0' && x <= '9'
-    then digitToInt x
-    else firstDigit xs
+firstDigitLetter [] _ v1 0 = v1 * 10 + v1
+firstDigitLetter [] _ v1 v2 = v1 * 10 + v2
+firstDigitLetter all@(_:xs) found v1 v2 =
+  if v /= 0
+    then if found
+           then firstDigitLetter xs True v1 v
+           else firstDigitLetter xs True v 0
+    else firstDigitLetter xs found v1 v2
+  where
+    v = startsWith all
 
 startsWith str
+  | isDigit = digitToInt x
   | "one" `isPrefixOf` str = 1
+  | "two" `isPrefixOf` str = 2
+  | "three" `isPrefixOf` str = 3
+  | "four" `isPrefixOf` str = 4
+  | "five" `isPrefixOf` str = 5
+  | "six" `isPrefixOf` str = 6
+  | "seven" `isPrefixOf` str = 7
+  | "eight" `isPrefixOf` str = 8
+  | "nine" `isPrefixOf` str = 9
   | otherwise = 0
+  where
+    (x:_) = str
+    isDigit = x >= '1' && x <= '9'
