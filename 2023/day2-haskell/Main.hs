9668e086bdb0091781b0a78e59e63359395c8bf0
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-25 22:58:55 +0100

    2023 day 2

--- 2023/day2-haskell/Main.hs
+++ 2023/day2-haskell/Main.hs
@@ -28,25 +28,26 @@ fn1 (x:xs)
   where
     (isPossible, id) = count x
 
-count line = (isPossibleList gamesStr, gameID)
+count line = (isPossible gamesStr, gameID)
   where
     (gameID, gamesStr) = parseGame line
 
-parseGame :: String -> (Int, [[String]])
-parseGame line = (gameID, gamesStr)
+parseGame :: String -> (Int, [(Int, String)])
+parseGame line = (gameID, games)
   where
     split = splitOn ": " line
     gameID = read (fromJust (stripPrefix "Game " (split !! 0))) :: Int
-    gamesStr = [splitOn ", " x | x <- splitOn "; " (split !! 1)]
+    gamesStr = concat [splitOn ", " x | x <- splitOn "; " (split !! 1)]
+    games = [parsePlay x | x <- gamesStr]
 
-isPossibleList :: [[String]] -> Bool
-isPossibleList [] = True
-isPossibleList (x:xs) =
-  if not (isPossible x)
-    then False
-    else isPossibleList xs
+parsePlay :: String -> (Int, String)
+parsePlay x = (count, color)
+  where
+    split = splitOn " " x
+    count = read (split !! 0) :: Int
+    color = split !! 1
 
-isPossible :: [String] -> Bool
+isPossible :: [(Int, String)] -> Bool
 isPossible [] = True
 isPossible (x:xs)
   | color == "red" =
@@ -62,33 +63,23 @@ isPossible (x:xs)
       then False
       else isPossible xs
   where
-    split = splitOn " " x
-    count = read (split !! 0) :: Int
-    color = split !! 1
+    (count, color) = x
 
 fn2 :: [String] -> Int
 fn2 [] = 0
 fn2 (x:xs) = count2 x + fn2 xs
 
 count2 line =
-  let (red, green, blue) = fewestList gamesStr 0 0 0
+  let (red, green, blue) = fewest gamesStr 0 0 0
    in red * green * blue
   where
     (_, gamesStr) = parseGame line
 
-fewestList :: [[String]] -> Int -> Int -> Int -> (Int, Int, Int)
-fewestList [] red green blue = (red, green, blue)
-fewestList (x:xs) red green blue = do
-  let (red2, green2, blue2) = fewest x 0 0 0
-  fewestList xs (max red red2) (max green green2) (max blue blue2)
-
-fewest :: [String] -> Int -> Int -> Int -> (Int, Int, Int)
+fewest :: [(Int, String)] -> Int -> Int -> Int -> (Int, Int, Int)
 fewest [] red green blue = (red, green, blue)
 fewest (x:xs) red green blue
   | color == "red" = fewest xs (max red count) green blue
   | color == "green" = fewest xs red (max green count) blue
   | color == "blue" = fewest xs red green (max blue count)
   where
-    split = splitOn " " x
-    count = read (split !! 0) :: Int
-    color = split !! 1
+    (count, color) = x
