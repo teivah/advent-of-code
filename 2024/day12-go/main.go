6a7bdcd618c407d12af6660dc0e3eec3c1342d9f
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2024-12-12 22:40:02 +0100

    Cleaning

--- 2024/day12-go/main.go
+++ 2024/day12-go/main.go
@@ -13,15 +13,34 @@ func fs1(input io.Reader) int {
 	visited := make(map[aoc.Position]bool)
 	res := 0
 	for pos, r := range board.Positions {
-		before := len(visited)
-		n := perimeter(r, board, pos, visited)
-		after := len(visited)
-		res += (after - before) * n
+		shape := make(map[aoc.Position]bool)
+		n := perimeter(r, board, pos, visited, shape)
+		res += len(shape) * n
 	}
 	return res
 }
 
-func perimeter(r rune, board aoc.Board[rune], pos aoc.Position, visited map[aoc.Position]bool) int {
+func fs2(input io.Reader) int {
+	board := aoc.NewBoardFromReader(input, func(row, col int, r rune) rune {
+		return r
+	})
+	visited := make(map[aoc.Position]bool)
+	res := 0
+	for pos, r := range board.Positions {
+		shape := make(map[aoc.Position]bool)
+		before := len(visited)
+		_ = perimeter(r, board, pos, visited, shape)
+		after := len(visited)
+		if after == before {
+			continue
+		}
+		count := countSides(board, shape)
+		res += (after - before) * count
+	}
+	return res
+}
+
+func perimeter(r rune, board aoc.Board[rune], pos aoc.Position, visited, shape map[aoc.Position]bool) int {
 	if !board.Contains(pos) {
 		return 1
 	}
@@ -35,31 +54,11 @@ func perimeter(r rune, board aoc.Board[rune], pos aoc.Position, visited map[aoc.
 		return 1
 	}
 	visited[pos] = true
-	return perimeter(r, board, pos.Move(aoc.Up, 1), visited) +
-		perimeter(r, board, pos.Move(aoc.Left, 1), visited) +
-		perimeter(r, board, pos.Move(aoc.Right, 1), visited) +
-		perimeter(r, board, pos.Move(aoc.Down, 1), visited)
-}
-
-func fs2(input io.Reader) int {
-	board := aoc.NewBoardFromReader(input, func(row, col int, r rune) rune {
-		return r
-	})
-	visited := make(map[aoc.Position]bool)
-	res := 0
-	for pos, r := range board.Positions {
-		internal := make(map[aoc.Position]bool)
-		external := make(map[aoc.Position]bool)
-		before := len(visited)
-		sides(r, board, pos, visited, internal, external)
-		after := len(visited)
-		if after == before {
-			continue
-		}
-		count := countSides(board, internal)
-		res += (after - before) * count
-	}
-	return res
+	shape[pos] = true
+	return perimeter(r, board, pos.Move(aoc.Up, 1), visited, shape) +
+		perimeter(r, board, pos.Move(aoc.Left, 1), visited, shape) +
+		perimeter(r, board, pos.Move(aoc.Right, 1), visited, shape) +
+		perimeter(r, board, pos.Move(aoc.Down, 1), visited, shape)
 }
 
 func countSides(board aoc.Board[rune], internal map[aoc.Position]bool) int {
@@ -68,15 +67,11 @@ func countSides(board aoc.Board[rune], internal map[aoc.Position]bool) int {
 	// From left
 	for col := 0; col < board.MaxCols; col++ {
 		for row := 0; row < board.MaxRows; row++ {
-			cur := aoc.NewPosition(row, col)
-			prev := aoc.NewPosition(row, col-1)
-			if internal[cur] && !internal[prev] {
+			if internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row, col-1)] {
 				count++
 				row++
 				for ; row < board.MaxRows; row++ {
-					cur := aoc.NewPosition(row, col)
-					prev := aoc.NewPosition(row, col-1)
-					if !(internal[cur] && !internal[prev]) {
+					if !(internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row, col-1)]) {
 						break
 					}
 				}
@@ -87,15 +82,11 @@ func countSides(board aoc.Board[rune], internal map[aoc.Position]bool) int {
 	// From right
 	for col := board.MaxCols - 1; col >= 0; col-- {
 		for row := 0; row < board.MaxRows; row++ {
-			cur := aoc.NewPosition(row, col)
-			prev := aoc.NewPosition(row, col+1)
-			if internal[cur] && !internal[prev] {
+			if internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row, col+1)] {
 				count++
 				row++
 				for ; row < board.MaxRows; row++ {
-					cur := aoc.NewPosition(row, col)
-					prev := aoc.NewPosition(row, col+1)
-					if !(internal[cur] && !internal[prev]) {
+					if !(internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row, col+1)]) {
 						break
 					}
 				}
@@ -106,15 +97,11 @@ func countSides(board aoc.Board[rune], internal map[aoc.Position]bool) int {
 	// From top
 	for row := 0; row < board.MaxRows; row++ {
 		for col := 0; col < board.MaxCols; col++ {
-			cur := aoc.NewPosition(row, col)
-			prev := aoc.NewPosition(row-1, col)
-			if internal[cur] && !internal[prev] {
+			if internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row-1, col)] {
 				count++
 				col++
 				for ; col < board.MaxCols; col++ {
-					cur := aoc.NewPosition(row, col)
-					prev := aoc.NewPosition(row-1, col)
-					if !(internal[cur] && !internal[prev]) {
+					if !(internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row-1, col)]) {
 						break
 					}
 				}
@@ -125,15 +112,11 @@ func countSides(board aoc.Board[rune], internal map[aoc.Position]bool) int {
 	// From bottom
 	for row := board.MaxRows - 1; row >= 0; row-- {
 		for col := 0; col < board.MaxCols; col++ {
-			cur := aoc.NewPosition(row, col)
-			prev := aoc.NewPosition(row+1, col)
-			if internal[cur] && !internal[prev] {
+			if internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row+1, col)] {
 				count++
 				col++
 				for ; col < board.MaxCols; col++ {
-					cur := aoc.NewPosition(row, col)
-					prev := aoc.NewPosition(row+1, col)
-					if !(internal[cur] && !internal[prev]) {
+					if !(internal[aoc.NewPosition(row, col)] && !internal[aoc.NewPosition(row+1, col)]) {
 						break
 					}
 				}
@@ -142,28 +125,4 @@ func countSides(board aoc.Board[rune], internal map[aoc.Position]bool) int {
 	}
 
 	return count
-}
-
-func sides(r rune, board aoc.Board[rune], pos aoc.Position, visited, internal, external map[aoc.Position]bool) {
-	if !board.Contains(pos) {
-		external[pos] = true
-		return
-	}
-	if visited[pos] {
-		if board.Get(pos) == r {
-			return
-		}
-		external[pos] = true
-		return
-	}
-	if r != board.Get(pos) {
-		external[pos] = true
-		return
-	}
-	visited[pos] = true
-	internal[pos] = true
-	sides(r, board, pos.Move(aoc.Up, 1), visited, internal, external)
-	sides(r, board, pos.Move(aoc.Left, 1), visited, internal, external)
-	sides(r, board, pos.Move(aoc.Right, 1), visited, internal, external)
-	sides(r, board, pos.Move(aoc.Down, 1), visited, internal, external)
 }
