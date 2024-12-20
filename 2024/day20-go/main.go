74a300ddf8cbb9edfb2c90c9238e84a2baf0b8be
teivah <teiva.harsanyi@gmail.com>
2024-12-20 18:15:15 +0100

    Close

--- 2024/day20-go/main.go
+++ 2024/day20-go/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/teivah/go-aoc"
@@ -227,6 +228,159 @@ func countCheats(all map[aoc.Position]int, board aoc.Board[cell], start, end aoc
 }
 
 func fs2(input io.Reader) int {
-	_ = aoc.ReaderToStrings(input)
-	return 42
+	var start, end aoc.Position
+	board := aoc.NewBoardFromReader(input, func(row, col int, r rune) cell {
+		switch r {
+		default:
+			panic(r)
+		case '.':
+			return track
+		case '#':
+			return wall
+		case 'S':
+			start = aoc.NewPosition(row, col)
+			return track
+		case 'E':
+			end = aoc.NewPosition(row, col)
+			return track
+		}
+	})
+	shortest := shortestPath(board, start, end)
+
+	all := make(map[aoc.Position]int)
+	for pos, c := range board.Positions {
+		if c != track {
+			continue
+		}
+		all[pos] = shortestPathWithAll(board, pos, end)
+	}
+	//offload(all)
+	//all := load()
+	return countCheats2(all, board, start, end, shortest)
+}
+
+type state3 struct {
+	moves             int
+	pos               aoc.Position
+	currentlyCheating bool
+	cheatingRemaining int
+	cheated           bool
+	cheatedPosStart   aoc.Position
+	cheatedPosEnd     aoc.Position
+	prevPos           aoc.Position
+}
+
+type key2 struct {
+	pos        aoc.Position
+	cheated    bool
+	cheating   bool
+	cheatStart aoc.Position
+	cheatEnd   aoc.Position
+}
+
+type cheat struct {
+	start aoc.Position
+	end   aoc.Position
+}
+
+/*
+s.cheatedPosStart.Row == 3 && s.cheatedPosStart.Col == 1 && s.cheatedPosEnd.Row == 7 && s.cheatedPosEnd.Col == 3
+s.cheatedPosStart.Row == 3 && s.cheatedPosStart.Col == 1 && s.pos.Row == 7 && s.pos.Col == 3
+*/
+func countCheats2(all map[aoc.Position]int, board aoc.Board[cell], start, end aoc.Position, shortest int) int {
+	q := []state3{
+		{pos: start},
+	}
+	visited := make(map[key2]bool)
+
+	res := make(map[cheat]bool)
+	count := make(map[int]int)
+	for len(q) != 0 {
+		s := q[0]
+		q = q[1:]
+		if s.pos == end {
+			if s.cheated {
+				saved := shortest - s.moves
+				if saved >= 50 && s.cheated {
+					k := cheat{start: s.cheatedPosStart, end: s.cheatedPosEnd}
+					if !res[k] {
+						res[k] = true
+						count[saved]++
+					}
+				}
+			}
+			continue
+		}
+
+		if s.moves >= shortest {
+			continue
+		}
+
+		k := key2{pos: s.pos, cheated: s.cheated, cheating: s.currentlyCheating, cheatStart: s.cheatedPosStart, cheatEnd: s.cheatedPosEnd}
+		if visited[k] {
+			continue
+		}
+		visited[k] = true
+		if !board.Contains(s.pos) {
+			continue
+		}
+		if board.Get(s.pos) == wall {
+			if s.cheated {
+				continue
+			}
+			if s.currentlyCheating {
+				s.cheatingRemaining--
+				if s.cheatingRemaining < 0 {
+					continue
+				}
+			} else {
+				s.currentlyCheating = true
+				s.cheatingRemaining = 19
+				s.cheatedPosStart = s.prevPos
+			}
+		} else {
+			if s.cheated {
+				panic("cheated")
+			} else if s.currentlyCheating {
+				s.cheated = true
+				s.currentlyCheating = false
+				s.cheatedPosEnd = s.pos
+
+				solution := s.moves + all[s.pos]
+
+				saved := shortest - solution
+				if saved >= 50 && s.cheated {
+					k := cheat{start: s.cheatedPosStart, end: s.cheatedPosEnd}
+					if !res[k] {
+						res[k] = true
+						count[saved]++
+					}
+				}
+				continue
+			}
+		}
+
+		s.moves++
+		pos := s.pos
+		s.prevPos = pos
+
+		s.pos = pos.Move(aoc.Up, 1)
+		q = append(q, s)
+		s.pos = pos.Move(aoc.Down, 1)
+		q = append(q, s)
+		s.pos = pos.Move(aoc.Left, 1)
+		q = append(q, s)
+		s.pos = pos.Move(aoc.Right, 1)
+		q = append(q, s)
+	}
+
+	ks := aoc.MapKeysToSlice(count)
+	sort.Slice(ks, func(i, j int) bool {
+		return ks[j] > ks[i]
+	})
+	for _, k := range ks {
+		fmt.Printf("There are %d that save %d\n", count[k], k)
+	}
+
+	return len(res)
 }
