07edfc2c10751719e3818837488ff3b6bcff8d95
teivah <teiva.harsanyi@gmail.com>
2024-12-21 18:56:06 +0100

    Start

--- 2024/day21-go/main.go
+++ 2024/day21-go/main.go
@@ -48,23 +48,35 @@ type button struct {
 	direction   aoc.Direction
 }
 
+var buttonA = button{isA: true}
+
 func fs1(input io.Reader) int {
 	codes := aoc.ReaderToStrings(input)
 	numKeypad := formatNumericKeypad()
-	robotA := getPosition(button{isA: true}, numKeypad)
+	robotA := numKeypad.buttons[buttonA]
 	dirKeypad := formatDirectionalKeypad()
+	res := 0
 	for _, code := range codes {
 		buttons := expectedButtons(code)
-		insRobotA := getInstructions(numKeypad, buttons, robotA)
 
-		robotB := getPosition(button{isA: true}, dirKeypad)
+		insRobotA := getInstructions(numKeypad, buttons, robotA)
+		robotB := dirKeypad.buttons[buttonA]
 		insRobotB := directionalRobotInstructions(robotB, dirKeypad, insRobotA)
-		fmt.Println(insRobotB)
+
+		robotC := dirKeypad.buttons[buttonA]
+		insRobotC := directionalRobotInstructions(robotC, dirKeypad, insRobotB)
+
+		robotD := dirKeypad.buttons[buttonA]
+		insRobotD := directionalRobotInstructions(robotD, dirKeypad, insRobotC)
+
+		iCode := code[:len(code)-1]
+		res += aoc.StringToInt(iCode) * len(insRobotD)
+		fmt.Println(iCode, len(insRobotD))
 	}
-	return 42
+	return res
 }
 
-func directionalRobotInstructions(pos aoc.Position, board aoc.Board[button], instructions []map[instruction]int) []map[instruction]int {
+func directionalRobotInstructions(pos aoc.Position, board Board, instructions []map[instruction]int) []map[instruction]int {
 	cur := pos
 	var res []map[instruction]int
 	for _, set := range instructions {
@@ -72,24 +84,26 @@ func directionalRobotInstructions(pos aoc.Position, board aoc.Board[button], ins
 			minDistance := math.MaxInt
 			var bestButton button
 			var best instruction
+			var bestCount int
 			var bestPosition aoc.Position
-			for ins := range set {
+			for ins, count := range set {
 				if len(set) > 1 && ins.isA {
 					continue
 				}
 				var b button
 				if ins.isA {
-					b = button{isA: true}
+					b = buttonA
 				} else {
 					b = button{isDirection: true, direction: ins.dir}
 				}
-				dst := getPosition(b, board)
+				dst := board.buttons[b]
 				v := cur.Manhattan(dst)
 				if v < minDistance {
 					minDistance = v
 					bestButton = b
 					best = ins
 					bestPosition = dst
+					bestCount = count
 				}
 				minDistance = min(minDistance, cur.Manhattan(dst))
 			}
@@ -98,63 +112,71 @@ func directionalRobotInstructions(pos aoc.Position, board aoc.Board[button], ins
 			}
 			ins := getInstructions(board, []button{bestButton}, cur)
 			res = append(res, ins...)
-			delete(set, best)
+			if bestCount == 1 {
+				delete(set, best)
+			} else {
+				set[best]--
+			}
 			cur = bestPosition
 		}
 	}
 	return res
 }
 
-func getInstructions(board aoc.Board[button], buttons []button, from aoc.Position) []map[instruction]int {
+func getInstructions(board Board, buttons []button, from aoc.Position) []map[instruction]int {
 	var instructions []map[instruction]int
 	cur := from
 	for _, b := range buttons {
 		row, col := delta(cur, b, board)
 		ins := instructionsFromDelta(cur, board, row, col)
 		instructions = append(instructions, ins)
-		cur = getPosition(b, board)
+		cur = board.buttons[b]
 	}
 	return instructions
 }
 
-func instructionsFromDelta(pos aoc.Position, board aoc.Board[button], row, col int) map[instruction]int {
+func instructionsFromDelta(pos aoc.Position, board Board, row, col int) map[instruction]int {
 	instructions := make(map[instruction]int)
 	cur := pos
 	for row != 0 {
 		if row < 0 {
 			p := cur.Move(aoc.Up, 1)
-			if contains(board, p) {
+			if board.contains(p) {
 				cur = p
 				instructions[instruction{isDir: true, dir: aoc.Up}]++
+				row++
 			} else {
 				if col < 0 {
 					cur = cur.Move(aoc.Left, 1)
 					instructions[instruction{isDir: true, dir: aoc.Left}]++
+					col++
 				} else if col > 0 {
 					cur = cur.Move(aoc.Right, 1)
 					instructions[instruction{isDir: true, dir: aoc.Right}]++
+					col--
 				} else {
 					panic("unknown")
 				}
 			}
-			row++
 		} else {
 			p := cur.Move(aoc.Down, 1)
-			if contains(board, p) {
+			if board.contains(p) {
 				cur = p
 				instructions[instruction{isDir: true, dir: aoc.Down}]++
+				row--
 			} else {
 				if col < 0 {
 					cur = cur.Move(aoc.Left, 1)
 					instructions[instruction{isDir: true, dir: aoc.Left}]++
+					col++
 				} else if col > 0 {
 					cur = cur.Move(aoc.Right, 1)
 					instructions[instruction{isDir: true, dir: aoc.Right}]++
+					col--
 				} else {
 					panic("unknown")
 				}
 			}
-			row--
 		}
 	}
 
@@ -173,18 +195,11 @@ func instructionsFromDelta(pos aoc.Position, board aoc.Board[button], row, col i
 	return instructions
 }
 
-func contains(board aoc.Board[button], pos aoc.Position) bool {
-	if !board.Contains(pos) {
-		return false
-	}
-	return board.Get(pos) != button{}
-}
-
 func expectedButtons(s string) []button {
 	var buttons []button
 	for _, c := range s {
 		if c == 'A' {
-			buttons = append(buttons, button{isA: true})
+			buttons = append(buttons, buttonA)
 			continue
 		}
 		buttons = append(buttons, button{isDigit: true, digit: aoc.RuneToInt(c)})
@@ -192,46 +207,58 @@ func expectedButtons(s string) []button {
 	return buttons
 }
 
-func getPosition(search button, board aoc.Board[button]) aoc.Position {
-	for p, b := range board.Positions {
-		if b == search {
-			return p
-		}
-	}
-	panic(search)
-}
-
-func delta(pos aoc.Position, to button, board aoc.Board[button]) (row, col int) {
-	toPos := getPosition(to, board)
+func delta(pos aoc.Position, to button, board Board) (row, col int) {
+	toPos := board.buttons[to]
 	return toPos.Row - pos.Row, toPos.Col - pos.Col
 }
 
-func formatNumericKeypad() aoc.Board[button] {
-	elements := []aoc.BoardElement[button]{
-		{T: button{isDigit: true, digit: 7}, Pos: aoc.NewPosition(0, 0)},
-		{T: button{isDigit: true, digit: 8}, Pos: aoc.NewPosition(0, 1)},
-		{T: button{isDigit: true, digit: 9}, Pos: aoc.NewPosition(0, 2)},
-		{T: button{isDigit: true, digit: 4}, Pos: aoc.NewPosition(1, 0)},
-		{T: button{isDigit: true, digit: 5}, Pos: aoc.NewPosition(1, 1)},
-		{T: button{isDigit: true, digit: 6}, Pos: aoc.NewPosition(1, 2)},
-		{T: button{isDigit: true, digit: 1}, Pos: aoc.NewPosition(2, 0)},
-		{T: button{isDigit: true, digit: 2}, Pos: aoc.NewPosition(2, 1)},
-		{T: button{isDigit: true, digit: 3}, Pos: aoc.NewPosition(2, 2)},
-		{T: button{isDigit: true, digit: 0}, Pos: aoc.NewPosition(3, 1)},
-		{T: button{isA: true}, Pos: aoc.NewPosition(3, 2)},
+func formatNumericKeypad() Board {
+	m := map[button]aoc.Position{
+		button{isDigit: true, digit: 7}: aoc.NewPosition(0, 0),
+		button{isDigit: true, digit: 8}: aoc.NewPosition(0, 1),
+		button{isDigit: true, digit: 9}: aoc.NewPosition(0, 2),
+		button{isDigit: true, digit: 4}: aoc.NewPosition(1, 0),
+		button{isDigit: true, digit: 5}: aoc.NewPosition(1, 1),
+		button{isDigit: true, digit: 6}: aoc.NewPosition(1, 2),
+		button{isDigit: true, digit: 1}: aoc.NewPosition(2, 0),
+		button{isDigit: true, digit: 2}: aoc.NewPosition(2, 1),
+		button{isDigit: true, digit: 3}: aoc.NewPosition(2, 2),
+		button{isDigit: true, digit: 0}: aoc.NewPosition(3, 1),
+		buttonA:                         aoc.NewPosition(3, 2),
 	}
-	return aoc.NewBoardFromElements(elements, 4, 3)
+	return newBoard(m)
 }
 
-func formatDirectionalKeypad() aoc.Board[button] {
-	elements := []aoc.BoardElement[button]{
-		{T: button{isDirection: true, direction: aoc.Up}, Pos: aoc.NewPosition(0, 1)},
-		{T: button{isA: true}, Pos: aoc.NewPosition(0, 2)},
-		{T: button{isDirection: true, direction: aoc.Left}, Pos: aoc.NewPosition(1, 0)},
-		{T: button{isDirection: true, direction: aoc.Down}, Pos: aoc.NewPosition(1, 1)},
-		{T: button{isDirection: true, direction: aoc.Right}, Pos: aoc.NewPosition(1, 2)},
+type Board struct {
+	buttons   map[button]aoc.Position
+	positions map[aoc.Position]button
+}
+
+func newBoard(buttons map[button]aoc.Position) Board {
+	positions := make(map[aoc.Position]button, len(buttons))
+	for b, p := range buttons {
+		positions[p] = b
 	}
-	return aoc.NewBoardFromElements(elements, 2, 3)
+	return Board{
+		buttons:   buttons,
+		positions: positions,
+	}
+}
+
+func (b Board) contains(pos aoc.Position) bool {
+	_, ok := b.positions[pos]
+	return ok
+}
+
+func formatDirectionalKeypad() Board {
+	m := map[button]aoc.Position{
+		button{isDirection: true, direction: aoc.Up}: aoc.NewPosition(0, 1),
+		buttonA: aoc.NewPosition(0, 2),
+		button{isDirection: true, direction: aoc.Left}:  aoc.NewPosition(1, 0),
+		button{isDirection: true, direction: aoc.Down}:  aoc.NewPosition(1, 1),
+		button{isDirection: true, direction: aoc.Right}: aoc.NewPosition(1, 2),
+	}
+	return newBoard(m)
 }
 
 func fs2(input io.Reader) int {
