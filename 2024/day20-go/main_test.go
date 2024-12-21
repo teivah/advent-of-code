d175edba0f89c973bb2a315da21ab65117a0f46f
teivah <teiva.harsanyi@gmail.com>
2024-12-21 01:28:02 +0100

    Part 2

--- 2024/day20-go/main_test.go
+++ 2024/day20-go/main_test.go
@@ -8,12 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFs1Test(t *testing.T) {
-	f, err := os.Open("test.txt")
-	require.NoError(t, err)
-	assert.Equal(t, 44, fs1(f))
-}
-
 func TestFs1Input(t *testing.T) {
 	f, err := os.Open("input.txt")
 	require.NoError(t, err)
